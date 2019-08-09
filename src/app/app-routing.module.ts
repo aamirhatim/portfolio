77ca16ef02505cd018736b6e263da066a0e31d2c
Aamir Husain <aamir300@gmail.com>
2019-08-09 16:29:10 -0400

    Add route for omni project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { OmniComponent } from './projects/omni/omni.component';
+
+const routes: Routes = [
+  { path: 'projects/omni', component: OmniComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
