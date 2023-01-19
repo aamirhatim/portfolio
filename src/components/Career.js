49599e8be83646e382b3271bce4452f588e182f7
Aamir Husain <aamir.husain@verizon.com>
2023-01-19 18:42:55 -0500

    template draft in progress

--- src/components/Career.js
+++ src/components/Career.js
@@ -1,9 +1,39 @@
 import React from 'react';
+import '../css/career.scss';
 
 export default function Career() {
     return(
         <div id='career' className='body-section'>
-            <h1>CAREER</h1>
+            <div className='job-template job-left'>
+                <div className='spacer'>
+                    <div className='job-info' style={{float: 'right'}}>
+                        <div>Role</div>
+                        <div>Company</div>
+                        <div>Location</div>
+                        <div>Duration</div>
+                        <div>Details</div>
+                    </div>
+                </div>
+                <div className='timeline-connector'>
+                    <div className='timeline-marker'></div>
+                </div>
+                <div className='spacer'></div>
+            </div>
+            <div className='job-template job-right'>
+                <div className='spacer'></div>
+                <div className='timeline-connector'>
+                    <div className='timeline-marker'></div>
+                </div>
+                <div className='spacer'>
+                    <div className='job-info'>
+                        <div>Role</div>
+                        <div>Company</div>
+                        <div>Location</div>
+                        <div>Duration</div>
+                        <div>Details</div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 };
