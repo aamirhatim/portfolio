85ddaf8245d4a4d8f8d8fadfb680374479272980
Aamir Husain <aamirhatim.h@gmail.com>
2023-01-19 22:26:35 -0500

    begin experimenting with reading from files to populate job tiles

--- src/components/Career.js
+++ src/components/Career.js
@@ -1,17 +1,44 @@
 import React from 'react';
 import '../css/career.scss';
 
+import CalendarIcon from '../assets/calendar_icon.svg';
+import LocationIcon from '../assets/location_icon.svg';
+
+// const yaml = require('js-yaml');
+// const fs = require('fs');
+
+try {
+    // var client = new XMLHttpRequest();
+    // client.open('GET', 'lib/jobs/verizon.yaml');
+    // client.onreadystatechange = function() {
+    //     console.log(client.responseText);
+    // }
+    // client.send();
+    
+    // const doc = yaml.load(fs.readFileSync('/src/lib/jobs/verizon.yaml', 'utf-8'));
+    // console.log(doc);
+} catch (e) {
+    console.log(e);
+}
+
 export default function Career() {
     return(
         <div id='career' className='body-section'>
             <div className='job-template job-left'>
                 <div className='spacer'>
                     <div className='job-info' style={{float: 'right'}}>
-                        <div>Role</div>
-                        <div>Company</div>
-                        <div>Location</div>
-                        <div>Duration</div>
-                        <div>Details</div>
+                        <div className='job-info-title'>
+                            <h3 className='job-info-role'>Robotics Architect @<span className='job-info-company'>Verizon</span></h3>
+                        </div>
+                        <div className='job-info-container'>
+                            <LocationIcon />
+                            <h4>New Jersey</h4>
+                        </div>
+                        <div className='job-info-container'>
+                            <CalendarIcon />
+                            <h4>2019-Now</h4>
+                        </div>
+                        <div className='job-info-details'>Details</div>
                     </div>
                 </div>
                 <div className='timeline-connector'>
@@ -26,11 +53,18 @@ export default function Career() {
                 </div>
                 <div className='spacer'>
                     <div className='job-info'>
-                        <div>Role</div>
-                        <div>Company</div>
-                        <div>Location</div>
-                        <div>Duration</div>
-                        <div>Details</div>
+                        <div className='job-info-title'>
+                            <h3 className='job-info-role'>Robotics Architect @<span clasName='job-info-company'>Verizon</span></h3>
+                        </div>
+                        <div className='job-info-container'>
+                            <LocationIcon />
+                            <h4>New Jersey</h4>
+                        </div>
+                        <div className='job-info-container'>
+                            <CalendarIcon />
+                            <h4>2019-Now</h4>
+                        </div>
+                        <div className='job-info-details'>Details</div>
                     </div>
                 </div>
             </div>
