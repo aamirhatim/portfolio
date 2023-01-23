524993feec25d8f2668509aacf4d90b69bf884e3
Aamir Husain <aamir.husain@verizon.com>
2023-01-23 15:30:15 -0500

    fix templates for job tiles

--- src/components/Career.js
+++ src/components/Career.js
@@ -1,73 +1,88 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import '../css/career.scss';
 
 import CalendarIcon from '../assets/calendar_icon.svg';
 import LocationIcon from '../assets/location_icon.svg';
 
-// const yaml = require('js-yaml');
-// const fs = require('fs');
+import JobsData from '../lib/jobs/jobs.json';
 
-try {
-    // var client = new XMLHttpRequest();
-    // client.open('GET', 'lib/jobs/verizon.yaml');
-    // client.onreadystatechange = function() {
-    //     console.log(client.responseText);
-    // }
-    // client.send();
-    
-    // const doc = yaml.load(fs.readFileSync('/src/lib/jobs/verizon.yaml', 'utf-8'));
-    // console.log(doc);
-} catch (e) {
-    console.log(e);
+function createJobTile(job, side) {
+    // Get div to insert tiles into
+    var body = document.getElementById('career');
+
+    // Get templates
+    var jobTemplate = document.getElementById('job-template');
+    var jobItem = jobTemplate.querySelector('.job', true);
+    var jobElement = document.importNode(jobItem, true);
+
+    var jobInfoTemplate = document.getElementById('job-info-template');
+    var jobInfoItem = jobInfoTemplate.querySelector('.job-info', true);
+    var jobInfoElement = document.importNode(jobInfoItem, true);
+
+    // Populate with job info
+    jobInfoElement.querySelector('.job-info-role').innerText = job.role;
+    jobInfoElement.querySelector('.job-info-company').inner = job.company;
+    jobInfoElement.querySelector('.job-info-location').innerText = job.location;
+
+    var jobDetails = jobInfoElement.querySelector('.job-info-details');
+    for ( var d in job.details ) {
+        var item = document.createElement('div');
+        item.innerText = job.details[d];
+        jobDetails.appendChild(item);
+    }
+
+    // Add job info to correct tile side
+    var spacers = jobElement.querySelectorAll('.spacer');
+    if ( side ) { // Load on left
+        spacers[0].appendChild(jobInfoElement);
+        jobInfoItem.style = {"float": "right"};
+    } else { // Load on right
+        spacers[1].appendChild(jobInfoElement);
+    }
+
+    // Add to DOM
+    body.appendChild(jobElement);
 }
 
 export default function Career() {
+    useEffect( () => {
+        // Iterate through job entires
+        var tileSide = true; // Start on the left side
+        for ( var job in JobsData ) {
+            createJobTile(JobsData[job], tileSide);
+            tileSide = !tileSide;
+        }
+    });
+
     return(
         <div id='career' className='body-section'>
-            <div className='job-template job-left'>
-                <div className='spacer'>
-                    <div className='job-info' style={{float: 'right'}}>
-                        <div className='job-info-title'>
-                            <h3 className='job-info-role'>Robotics Architect @<span className='job-info-company'>Verizon</span></h3>
-                        </div>
-                        <div className='job-info-container'>
-                            <LocationIcon />
-                            <h4>New Jersey</h4>
-                        </div>
-                        <div className='job-info-container'>
-                            <CalendarIcon />
-                            <h4>2019-Now</h4>
-                        </div>
-                        <div className='job-info-details'>Details</div>
+            <template id='job-template'>
+                <div className='job'>
+                    <div className='spacer'></div>
+                    <div className='timeline-connector'>
+                        <div className='timeline-marker'></div>
                     </div>
+                    <div className='spacer'></div>
                 </div>
-                <div className='timeline-connector'>
-                    <div className='timeline-marker'></div>
-                </div>
-                <div className='spacer'></div>
-            </div>
-            <div className='job-template job-right'>
-                <div className='spacer'></div>
-                <div className='timeline-connector'>
-                    <div className='timeline-marker'></div>
-                </div>
-                <div className='spacer'>
-                    <div className='job-info'>
-                        <div className='job-info-title'>
-                            <h3 className='job-info-role'>Robotics Architect @<span clasName='job-info-company'>Verizon</span></h3>
-                        </div>
-                        <div className='job-info-container'>
-                            <LocationIcon />
-                            <h4>New Jersey</h4>
-                        </div>
-                        <div className='job-info-container'>
-                            <CalendarIcon />
-                            <h4>2019-Now</h4>
-                        </div>
-                        <div className='job-info-details'>Details</div>
+            </template>
+
+            <template id='job-info-template'>
+                <div className='job-info'>
+                    <div className='job-info-title'>
+                        <h3 className='job-info-role'></h3>
+                        <h3 className='job-info-company'></h3>
                     </div>
+                    <div className='job-info-container'>
+                        <LocationIcon />
+                        <h4 className='job-info-location'></h4>
+                    </div>
+                    <div className='job-info-container'>
+                        <CalendarIcon />
+                        <h4 className='job-info-date'>DATE</h4>
+                    </div>
+                    <div className='job-info-details'></div>
                 </div>
-            </div>
+            </template>
         </div>
     )
 };
