b1d4bd293d99edbf4b7fa645601d4134046ca6d8
Aamir Husain <aamirhatim.h@gmail.com>
2023-01-30 14:20:07 -0500

    Add projects and create dynamic tile creation scripts

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,7 +1,9 @@
 import React, { useEffect } from 'react';
 import '../css/projects.scss';
 
-function getProjectTile() {
+import ProjectData from '../lib/projects/project_summary.json';
+
+function getProjectTile(p) {
     // Get parent div
     var projects = document.getElementById('proj-container');
 
@@ -11,9 +13,22 @@ function getProjectTile() {
     var projElement = document.importNode(projItem, true);
 
     // Populate
-    projElement.querySelector('.proj-title').innerText = "Project Title";
-    projElement.querySelector('.proj-subtitle').innerText = "Project description";
-    projElement.querySelector('.proj-description').innerText = "This is a project description about a project. It is pretty interesting."
+    projElement.querySelector('.proj-title').innerText = p.title;
+    projElement.querySelector('.proj-subtitle').innerText = p.subtitle;
+    projElement.querySelector('.proj-description').innerText = p.description;
+
+    if ( p.select ) {
+        projElement.classList.add('select');
+    }
+
+    // Add skills
+    var skills = projElement.querySelector('.proj-skills');
+    for (var s in p.skills) {
+        var skill = document.createElement('div');
+        skill.classList.add('skill');
+        skill.innerText = p.skills[s];
+        skills.appendChild(skill);
+    }
 
     // Add to DOM
     projects.appendChild(projElement);
@@ -24,21 +39,30 @@ export default function Project() {
         // Clear out projects section before populating
         document.getElementById('proj-container').innerHTML= null;
         
-        for (var x = 0; x <= 1; x++) {
-            getProjectTile();
+        // Iterate through project data
+        for (var p in ProjectData) {
+            getProjectTile(ProjectData[p]);
         }
     });
 
     return(
         <div id='projects' className='body-section'>
-            <h1>PROJECTS</h1>
+            {/* <h1>PROJECTS</h1> */}
             <div id='proj-container'></div>
 
             <template id='project-template'>
                 <div className='project'>
-                    <h4 className='proj-title'></h4>
-                    <h5 className='proj-subtitle'></h5>
-                    <p className='proj-description'></p>
+                    <div>
+                        <h4 className='proj-title'></h4>
+                        <h5 className='proj-subtitle'></h5>
+                        <p className='proj-description'></p>
+                        <div>
+                            <button>Read more</button>
+                            {/* Github Icon */}
+                            {/* Video Icon */}
+                        </div>
+                    </div>
+                    <div className='proj-skills'></div>
                 </div>
             </template>
         </div>
