da88c2bedeb5a413b73a65095bced46d643fadcb
Aamir Husain <aamirhatim.h@gmail.com>
2023-01-30 17:23:33 -0500

    add stickers

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -5,25 +5,29 @@ import concepts from '../lib/concepts.html';
 import code from '../lib/code.html';
 import tools from '../lib/tools.html';
 
+import ConceptsStickers from '../assets/concepts_stickers.svg';
+import CodeStickers from '../assets/code_stickers.svg';
+import ToolStickers from '../assets/tool_stickers.svg';
+
 export default function Skills() {
     return(
         <div id='skills' className='body-section'>
-            <div id='skills-concepts' className='skills-section'>
-                <div className='skills-stickers'></div>
+            <div className='skills-section'>
+                <ConceptsStickers />
                 <div className='skills-text'>
                     <h2>CONCEPTS</h2>
                     <h3 dangerouslySetInnerHTML={{__html: concepts}}></h3>
                 </div>
             </div>
-            <div id='skills-code' className='skills-section'>
-                <div className='skills-stickers'></div>
+            <div className='skills-section'>
+                <CodeStickers />
                 <div className='skills-text'>
                     <h2>CODE</h2>
                     <h3 dangerouslySetInnerHTML={{__html: code}}></h3>
                 </div>
             </div>
-            <div id='skills-tools' className='skills-section'>
-                <div className='skills-stickers'></div>
+            <div className='skills-section'>
+                <ToolStickers />
                 <div className='skills-text'>
                     <h2>TOOLS</h2>
                     <h3 dangerouslySetInnerHTML={{__html: tools}}></h3>
