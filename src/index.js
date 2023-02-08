be5c238aab94c91ce19d3e4605d02851fb2eebb5
Aamir Husain <aamirhatim.h@gmail.com>
2023-02-07 23:57:53 -0500

    use top level App component. shift children to App component code

--- src/index.js
+++ src/index.js
@@ -1,26 +1,10 @@
 import {createRoot } from 'react-dom/client';
 import React from 'react';
 
-import About from './components/About';
-import Career from './components/Career';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
-import Nav from './components/Nav';
-import Projects from './components/Projects';
-import Skills from './components/Skills';
-
-import './css/index.scss';
+import App from './components/App';
 
 const container = document.getElementById('app');
 const root = createRoot(container);
 root.render(
-    <React.StrictMode>
-        <Nav />
-        <About />
-        <Skills />
-        <Career />
-        <Projects />
-        <Contact />
-        <Footer />
-    </React.StrictMode>
+    <App />
 );
