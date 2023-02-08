3dd86f4d4355f1fca1dc4a675dbc5fea47dc4909
Aamir Husain <aamirhatim.h@gmail.com>
2023-02-07 23:55:45 -0500

    Create top level component for better design

---
+++ src/components/App.js
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from 'react';
+import About from './About';
+import Career from './Career';
+import Contact from './Contact';
+import Footer from './Footer';
+import Nav from './Nav';
+import Projects from './Projects';
+import Skills from './Skills';
+import '../css/index.scss';
+import ThemeContext from '../context/ThemeContext';
+
+export default function App() {
+    const [theme, setTheme] = useState('light');
+    const applyTheme = (e) => {
+        if ( theme == 'dark' ) {
+            for (var i in e ) {
+                e[i].classList.add('dark');
+            }
+        } else {
+            for (var i in e ) {
+                e[i].classList.remove('dark');
+            }
+        }
+    }
+
+    const value = {theme, setTheme, applyTheme};
+    // useEffect( () => {
+    //     var d = new Date();
+    //     if ( d.getHours() >= 17 ) {
+    //         setDarkMode(true);
+    //         console.log(darkMode);
+    //     }
+    // });
+
+    return(
+        <React.StrictMode>
+            <ThemeContext.Provider value={value}>
+                <Nav />
+                <About />
+                <Skills />
+                <Career />
+                <Projects />
+                <Contact />
+                <Footer />
+            </ThemeContext.Provider>
+        </React.StrictMode>
+    )
+};
