9091a56e930cb08b91155880f0514b3d93fb9599
Aamir Husain <aamirhatim.h@gmail.com>
2023-02-10 09:26:31 -0500

    add dark mode to skills section

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import '../css/skills.scss';
+import ThemeContext from '../context/ThemeContext';
 
 import concepts from '../lib/skills/concepts.html';
 import code from '../lib/skills/code.html';
@@ -10,24 +11,34 @@ import CodeStickers from '../assets/code_stickers.svg';
 import ToolStickers from '../assets/tool_stickers.svg';
 
 export default function Skills() {
+    const {theme, setTheme, applyTheme} = React.useContext(ThemeContext);
+    useEffect( () => {
+        // Apply theme
+        var themeSet = [
+            document.getElementById('skills'),
+            Array.from(document.getElementsByClassName('skills-stickers'))
+        ];
+        applyTheme(themeSet.flat(Infinity));
+    });
+
     return(
         <div id='skills' className='body-section'>
             <div className='skills-section'>
-                <ConceptsStickers />
+                <ConceptsStickers className='skills-stickers' />
                 <div className='skills-text'>
                     <h2>Concepts</h2>
                     <h3 dangerouslySetInnerHTML={{__html: concepts}}></h3>
                 </div>
             </div>
             <div className='skills-section'>
-                <CodeStickers />
+                <CodeStickers className='skills-stickers' />
                 <div className='skills-text'>
                     <h2>Code</h2>
                     <h3 dangerouslySetInnerHTML={{__html: code}}></h3>
                 </div>
             </div>
             <div className='skills-section'>
-                <ToolStickers />
+                <ToolStickers className='skills-stickers' />
                 <div className='skills-text'>
                     <h2>Tools</h2>
                     <h3 dangerouslySetInnerHTML={{__html: tools}}></h3>
