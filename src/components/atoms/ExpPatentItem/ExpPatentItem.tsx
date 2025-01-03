8c6d43ee204e7d5bd02341ca72bf0cb124d35ff9
Aamir Husain <aamirhatim.h@gmail.com>
2025-01-02 23:12:23 -0500

    show year on new line

--- src/components/atoms/ExpPatentItem/ExpPatentItem.tsx
+++ src/components/atoms/ExpPatentItem/ExpPatentItem.tsx
@@ -5,9 +5,10 @@ function ExpPatentItem(props: {item: Patent}) {
     return (
         <div className='exp-patent-item'>
             <div className='patent-title'>
-                <div>{props.item.year} - {props.item.title}</div>
+                <div>{props.item.title}</div>
                 <div className='patent-status'>{props.item.status}</div>
             </div>
+            <div className='patent-description'>{props.item.year}</div>
             <div className='patent-description'>{props.item.description}</div>
         </div> 
     )
