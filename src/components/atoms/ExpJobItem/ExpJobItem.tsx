7309f71ed7d575822899fe1ae8057723a0661ea4
Aamir Husain <aamirhatim.h@gmail.com>
2025-01-02 23:13:55 -0500

    add years for prev jobs

--- src/components/atoms/ExpJobItem/ExpJobItem.tsx
+++ src/components/atoms/ExpJobItem/ExpJobItem.tsx
@@ -5,6 +5,7 @@ function ExpJobItem(props: {job: Job}) {
     return (
         <div className='exp-job-item'>
            <div className='job-title'>{props.job.title} <span className='job-company'>@{props.job.company}</span></div>
+           <div className='job-description'>{props.job.start} - {props.job.end}</div>
            <div className='job-description'>{props.job.description}</div>
         </div> 
     )
