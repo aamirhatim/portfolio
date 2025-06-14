1172218649d9fd53a77ca02592f1ef187efa2d46
Aamir Husain <aamirhatim.h@gmail.com>
2025-06-14 19:06:16 -0400

    add home button

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,9 +1,11 @@
 import { createRootRoute, Outlet } from '@tanstack/react-router'
 import Navbar from '../components/molecules/Navbar/navbar'
+import HomeButton from '../components/atoms/HomeButton/homeButton'
 
 export const Route = createRootRoute({
     component: () => (
         <>
+            <HomeButton />
             <div id="route-outlet">
                 <Outlet />
             </div>
