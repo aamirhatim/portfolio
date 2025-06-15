9e2a4cb2e03363a8675d6c4ab44412aa9372c054
Aamir Husain <aamirhatim.h@gmail.com>
2025-06-15 13:37:54 -0400

    update style to look like a floating pill

--- src/components/molecules/Navbar/navbar.tsx
+++ src/components/molecules/Navbar/navbar.tsx
@@ -1,4 +1,5 @@
-import { Link } from "@tanstack/react-router";
+import { Link } from '@tanstack/react-router';
+import HomeButton from '../../atoms/HomeButton/homeButton';
 
 export default function Navbar() {
     const navItems = [
@@ -8,12 +9,17 @@ export default function Navbar() {
         'connect'
     ]
     return (
-        <nav>
-            {
-                navItems.map( (n, idx) => (
-                    <Link key={idx} to={n}>{n.toUpperCase()}</Link>
-                ))
-            }
+        <nav className='fixed top-0 left-0 box-border w-dvw p-5'>
+            <div className='w-full box-border px-6 py-3 flex border rounded-full bg-black text-l text-white'>
+                <HomeButton />
+                <div className='flex grow justify-end gap-8 h-full'>
+                    {
+                        navItems.map( (n, idx) => (
+                            <Link key={idx} to={n} className='content-center'>{n}</Link>
+                        ))
+                    }
+                </div>
+            </div>
         </nav>
     )
 }
