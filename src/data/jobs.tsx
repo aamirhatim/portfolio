a4784389c225cc756978b1c9e5ab3b5ccd005815
Aamir Husain <aamirhatim.h@gmail.com>
2025-06-15 14:15:30 -0400

    add current work description

--- src/data/jobs.tsx
+++ src/data/jobs.tsx
@@ -1,5 +1,13 @@
 import { Job } from './datatypes'
 
+export const currentWorkDesc = 
+    "I'm a software developer \
+    primarily researching novel applications that use or enable 5G, edge compute, \
+    artificial intelligence and robotics. I've worked with a wide \
+    range of robots that include indoor/outdoor AMRs, pallet movers, \
+    telepresence AMRs, quadrupeds, arms, drones, educational robots \
+    and autonomous vehicles."
+
 export const jobs: Array<Job> = [
     {
         title: "Distinguished Engineer",
