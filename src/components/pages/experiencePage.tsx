663e149bc6803585feef3fdf21a854fdecb8bb99
Aamir Husain <aamirhatim.h@gmail.com>
2025-06-15 14:13:30 -0400

    Add all experience sections

--- src/components/pages/experiencePage.tsx
+++ src/components/pages/experiencePage.tsx
@@ -1,5 +1,15 @@
+import CurrentWork from "../molecules/currentWork";
+import Patents from "../molecules/patents";
+import PrevWork from "../molecules/prevWork";
+import Schooling from "../molecules/schooling";
+
 export default function ExperiencePage() {
     return (
-        <></>
+        <div className='flex flex-col gap-10'>
+            <CurrentWork />
+            <PrevWork />
+            <Patents />
+            <Schooling />
+        </div>
     )
 }
