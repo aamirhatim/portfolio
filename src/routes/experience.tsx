c2aff38b61a225bebb08881ecb6a7b9385bc373b
Aamir Husain <aamirhatim.h@gmail.com>
2025-06-15 13:45:32 -0400

    load experience page into route

--- src/routes/experience.tsx
+++ src/routes/experience.tsx
@@ -1,9 +1,10 @@
 import { createFileRoute } from '@tanstack/react-router'
+import ExperiencePage from '../components/pages/experiencePage'
 
 export const Route = createFileRoute('/experience')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <div>Hello "/experience"!</div>
+  return <ExperiencePage />
 }
