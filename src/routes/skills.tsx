e6797d0a929ddd13d5d3e828b523751b4d073a8d
Aamir Husain <aamirhatim.h@gmail.com>
2025-06-15 13:40:21 -0400

    Add skills page into route

--- src/routes/skills.tsx
+++ src/routes/skills.tsx
@@ -1,9 +1,10 @@
 import { createFileRoute } from '@tanstack/react-router'
+import SkillsPage from '../components/pages/skillsPage'
 
 export const Route = createFileRoute('/skills')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <div>Hello "/skills"!</div>
+  return <SkillsPage />
 }
