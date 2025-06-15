3cfde20cf4ab0c9a52371a501462a837b4a07950
Aamir Husain <aamirhatim.h@gmail.com>
2025-06-15 13:40:50 -0400

    create new skills page that wil replace old one

---
+++ src/components/pages/skillsPage.tsx
@@ -0,0 +1,32 @@
+import { shuffle } from 'lodash'
+import { Skill } from '../../data/datatypes'
+import { skillSet, skillDescriptions } from '../../data/skillsData'
+import SkillItem from '../atoms/SkillItem/SkillItem'
+import SkillType from '../atoms/SkillType/SkillType'
+
+function createSkill(skill: Skill) {
+    const key = 'skill-' + skill.value.toLowerCase().replace(' ', '-')
+
+    return(
+        <SkillItem key={key} value={skill.value} type={skill.type} />
+    )
+}
+
+export default function SkillsPage() {
+    // Shuffle skills
+    const skillsShuffle = shuffle(skillSet)
+
+    return (
+        <>
+            <div className='mb-15 flex flex-wrap justify-center gap-8'>
+                <SkillType title='Code' description={skillDescriptions.code} />
+                <SkillType title='Concepts' description={skillDescriptions.concepts} />
+                <SkillType title='Tools' description={skillDescriptions.tools} />
+            </div>
+            
+            <div className='flex flex-wrap justify-center gap-2'>
+                { skillsShuffle.map( (s) => createSkill(s) ) }
+            </div>
+        </>
+    )
+}
