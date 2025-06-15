b8b70ccb88a9e787e9f6ae8f178644e0d53722e5
Aamir Husain <aamirhatim.h@gmail.com>
2025-06-15 13:42:11 -0400

    add navbar and styling

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,15 +1,13 @@
 import { createRootRoute, Outlet } from '@tanstack/react-router'
-import Navbar from '../components/molecules/Navbar/navbar'
-import HomeButton from '../components/atoms/HomeButton/homeButton'
+import Navbar from '../components/molecules/navbar'
 
 export const Route = createRootRoute({
     component: () => (
-        <>
-            <HomeButton />
-            <div id="route-outlet">
+        <div id='app-container' className='min-h-dvh w-dvw box-border flex flex-col bg-red-950'>
+            <Navbar />
+            <div id='route-outlet' className='box-border h-full pt-40 pb-15 px-12'>
                 <Outlet />
             </div>
-            <Navbar />
-        </>
+        </div>
     )
 })
