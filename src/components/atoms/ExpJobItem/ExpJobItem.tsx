130779f32d663bf0fd60ca6fc23a39252f6163d5
Aamir Husain <aamirhatim.h@gmail.com>
2025-06-15 20:58:58 -0400

    use tailwind

--- src/components/atoms/ExpJobItem/ExpJobItem.tsx
+++ src/components/atoms/ExpJobItem/ExpJobItem.tsx
@@ -1,12 +1,11 @@
 import { Job } from '../../../data/datatypes'
-import './ExpJobItem.style.scss'
 
 function ExpJobItem(props: {job: Job}) {
     return (
-        <div className='exp-job-item'>
-           <div className='job-title'>{props.job.title} <span className='job-company'>@{props.job.company}</span></div>
-           <div className='job-description'>{props.job.start} - {props.job.end}</div>
-           <div className='job-description'>{props.job.description}</div>
+        <div>
+           <div className='text-2xl font-bold'>{props.job.title} <span>@{props.job.company}</span></div>
+           <div className='text-lg'>{props.job.start} - {props.job.end}</div>
+           <div className='text-xl'>{props.job.description}</div>
         </div> 
     )
 }
