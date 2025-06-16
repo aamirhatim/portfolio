7221a67122105a2bd8c32c5cde5242c150fbf790
Aamir Husain <aamirhatim.h@gmail.com>
2025-06-15 21:18:47 -0400

    style tweak

--- src/components/atoms/ExpPatentItem/ExpPatentItem.tsx
+++ src/components/atoms/ExpPatentItem/ExpPatentItem.tsx
@@ -2,7 +2,7 @@ import { Patent } from '../../../data/datatypes'
 
 function ExpPatentItem(props: {item: Patent}) {
     return (
-        <div className='box-border p-5 border rounded-xl flex flex-col justify-start items-center w-75 gap-4'>
+        <div className='box-border p-5 border rounded-xl flex flex-col justify-start items-center w-70 gap-4'>
             <div className='box-border border rounded-full text-sm font-bold px-2'>{props.item.status}</div>
             <div className='text-center text-xl font-bold'>{props.item.title}</div>
             <div className='text-lg'>{props.item.description}</div>
