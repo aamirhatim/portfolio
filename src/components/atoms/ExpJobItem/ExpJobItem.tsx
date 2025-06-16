f7fbf7c057e35b8ccba3b1ff2e0dc52b0b28573c
Aamir Husain <aamirhatim.h@gmail.com>
2025-06-15 21:42:39 -0400

    put date first

--- src/components/atoms/ExpJobItem/ExpJobItem.tsx
+++ src/components/atoms/ExpJobItem/ExpJobItem.tsx
@@ -3,8 +3,8 @@ import { Job } from '../../../data/datatypes'
 export default function ExpJobItem(props: {job: Job}) {
     return (
         <div>
-            <div className='text-2xl font-bold'>{props.job.title} <span>@{props.job.company}</span></div>
             <div className='text-lg'>{props.job.start} - {props.job.end}</div>
+            <div className='text-2xl font-bold'>{props.job.title} <span>@{props.job.company}</span></div>
             <div className='text-xl'>{props.job.description}</div>
             {props.job.skills &&
                 <div className='flex flex-wrap gap-1 mt-2'>
--- src/components/molecules/currentWork.tsx
+++ src/components/molecules/currentWork.tsx
@@ -3,8 +3,8 @@ import { currentWorkDesc, jobs } from "../../data/jobs";
 export default function CurrentWork() {
     return (
         <section className='box-border border rounded-xl p-5'>
-            <div className='text-4xl font-bold'>{jobs[0].title} <span>@{jobs[0].company}</span></div>
             <div className='text-xl'>{jobs[0].start} - Present</div>
+            <div className='text-4xl font-bold'>{jobs[0].title} <span>@{jobs[0].company}</span></div>
             <div className='box-border mt-4 text-2xl'>{currentWorkDesc}</div>
         </section>
     )
