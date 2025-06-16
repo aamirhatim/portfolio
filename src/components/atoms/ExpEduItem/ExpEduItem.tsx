bafcfaee0214409f645d8704e557a64f37e866b6
Aamir Husain <aamirhatim.h@gmail.com>
2025-06-15 21:19:38 -0400

    switch to tailwind

--- src/components/atoms/ExpEduItem/ExpEduItem.tsx
+++ src/components/atoms/ExpEduItem/ExpEduItem.tsx
@@ -1,13 +1,10 @@
 import { Education } from '../../../data/datatypes'
-import './ExpEduItem.style.scss'
 
-function ExpEduItem(props: {item:Education}){
+export default function ExpEduItem(props: {item:Education}){
     return (
-        <div className='exp-edu-item'>
-            <div className='exp-edu-degree'>{props.item.degree}: <span className='edu-major'>{props.item.major}</span></div>
-            <div className='exp-edu-school'>{props.item.school} - {props.item.yearEnd}</div>
+        <div>
+            <div className='text-2xl font-bold'>{props.item.degree}: <span>{props.item.major}</span></div>
+            <div className='text-xl'>{props.item.school} - {props.item.yearEnd}</div>
         </div>
     )
 }
-
-export default ExpEduItem
