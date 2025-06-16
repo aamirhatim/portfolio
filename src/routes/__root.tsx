d1b831a1b4507a018e9a9d4a6edd35b1d3f10aec
Aamir Husain <aamirhatim.h@gmail.com>
2025-06-15 21:18:26 -0400

    use percentage for outlet padding. Remove div IDs. Remove bg color

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -3,9 +3,9 @@ import Navbar from '../components/molecules/navbar'
 
 export const Route = createRootRoute({
     component: () => (
-        <div id='app-container' className='min-h-dvh w-dvw box-border flex flex-col bg-red-950'>
+        <div className='min-h-dvh w-dvw box-border flex flex-col'>
             <Navbar />
-            <div id='route-outlet' className='box-border h-full pt-40 pb-15 px-12'>
+            <div className='box-border h-full pt-40 pb-15 px-[15%]'>
                 <Outlet />
             </div>
         </div>
