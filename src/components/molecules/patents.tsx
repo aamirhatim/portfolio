4d9abea07e5cc9a414e7c1133953466996efde7c
Aamir Husain <aamirhatim.h@gmail.com>
2025-06-19 17:10:30 -0400

    remove styling

--- src/components/molecules/patents.tsx
+++ src/components/molecules/patents.tsx
@@ -9,7 +9,7 @@ function createPatentItem(patent:Patent) {
 
 export default function Patents() {
     return (
-        <section className='box-border p-5'>
+        <section>
             <div className='text-4xl font-bold mb-6'>Patents</div>
             <div className='flex flex-wrap gap-6 justify-center'>
                 {patents.map( (p) => createPatentItem(p) )}
