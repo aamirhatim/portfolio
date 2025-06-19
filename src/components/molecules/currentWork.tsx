72bbccf8a2685f17df82a953162e57ac60a81a78
Aamir Husain <aamirhatim.h@gmail.com>
2025-06-19 17:00:50 -0400

    space things out a bit more

--- src/components/molecules/currentWork.tsx
+++ src/components/molecules/currentWork.tsx
@@ -2,7 +2,7 @@ import { currentWorkDesc, jobs } from "../../data/jobs";
 
 export default function CurrentWork() {
     return (
-        <section className='box-border border rounded-xl p-5'>
+        <section className='box-border border rounded-xl p-15'>
             <div className='text-xl'>{jobs[0].start} - Present</div>
             <div className='text-4xl font-bold'>{jobs[0].title} <span>@{jobs[0].company}</span></div>
             <div className='box-border mt-4 text-2xl'>{currentWorkDesc}</div>
