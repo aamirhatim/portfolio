41a4c516c820df14a0371270affee546cacafd18
Aamir Husain <aamirhatim.h@gmail.com>
2025-06-19 21:39:14 -0400

    implement tailwind and remove color prop

--- src/components/atoms/ProjLinkChip/ProjLinkChip.tsx
+++ src/components/atoms/ProjLinkChip/ProjLinkChip.tsx
@@ -1,9 +1,5 @@
-import './ProjLinkChip.style.scss'
-
-function ProjLinkChip(props: {value:string, url:string, color:string}) {
+export default function ProjLinkChip(props: {value:string, url:string}) {
     return (
-        <div className='proj-link-chip' style={{ backgroundColor: props.color }}><a href={props.url} target='_blank'>{props.value}</a></div>
+        <div className='text-md font-bold'><a href={props.url} target='_blank'>{props.value}</a></div>
     )
 }
-
-export default ProjLinkChip
