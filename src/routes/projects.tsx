b9c312d9a0ca5ac82fd9b3bf3de0756f85952916
Aamir Husain <aamirhatim.h@gmail.com>
2025-06-19 21:40:00 -0400

    add projetcs component

--- src/routes/projects.tsx
+++ src/routes/projects.tsx
@@ -1,9 +1,10 @@
 import { createFileRoute } from '@tanstack/react-router'
+import ProjectsPage from '../components/pages/projectsPage'
 
 export const Route = createFileRoute('/projects')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <div>Hello "/projects"!</div>
+  return <ProjectsPage />
 }
