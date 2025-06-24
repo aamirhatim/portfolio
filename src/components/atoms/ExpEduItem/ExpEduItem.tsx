a778e2132ed0338f06c52c7093189d8324bfc27a
Aamir Husain <aamirhatim.h@gmail.com>
2025-06-23 21:50:46 -0400

    make font smaller

--- src/components/atoms/ExpEduItem/ExpEduItem.tsx
+++ src/components/atoms/ExpEduItem/ExpEduItem.tsx
@@ -3,8 +3,8 @@ import { Education } from '../../../data/datatypes'
 export default function ExpEduItem(props: {item:Education}){
     return (
         <div>
-            <div className='text-2xl font-bold'>{props.item.degree}: <span>{props.item.major}</span></div>
-            <div className='text-xl'>{props.item.school} - {props.item.yearEnd}</div>
+            <div className='text-xl font-bold'>{props.item.degree}: <span>{props.item.major}</span></div>
+            <div className='text-lg'>{props.item.school} - {props.item.yearEnd}</div>
         </div>
     )
 }
