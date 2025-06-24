069125b949d0dadf0a2db664f981a2a716a2cce6
Aamir Husain <aamirhatim.h@gmail.com>
2025-06-23 21:52:05 -0400

    make font smaller

--- src/components/molecules/currentWork.tsx
+++ src/components/molecules/currentWork.tsx
@@ -2,10 +2,10 @@ import { currentWorkDesc, jobs } from "../../data/jobs";
 
 export default function CurrentWork() {
     return (
-        <section className='box-border border rounded-xl p-15'>
-            <div className='text-xl'>{jobs[0].start} - Present</div>
-            <div className='text-4xl font-bold'>{jobs[0].title} <span>@{jobs[0].company}</span></div>
-            <div className='box-border mt-4 text-2xl'>{currentWorkDesc}</div>
+        <section className='box-border border rounded-xl p-10'>
+            <div className='text-lg'>{jobs[0].start} - Present</div>
+            <div className='text-3xl font-bold'>{jobs[0].title} <span>@{jobs[0].company}</span></div>
+            <div className='box-border mt-4 text-xl'>{currentWorkDesc}</div>
         </section>
     )
 }
