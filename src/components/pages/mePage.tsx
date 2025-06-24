3c15dde7299671da3f70e52eccdf4afb70e073df
Aamir Husain <aamirhatim.h@gmail.com>
2025-06-23 22:10:50 -0400

    add placeholders

--- src/components/pages/mePage.tsx
+++ src/components/pages/mePage.tsx
@@ -16,9 +16,20 @@ export default function MePage() {
     const skillsShuffle = shuffle(skillSet)
 
     return (
-        <div className="flex flex-col gap-30">
-            <section className='flex flex-wrap gap-2'>
-                { skillsShuffle.map( (s) => createSkill(s) ) }
+        <div className="flex flex-col gap-20">
+            <section>
+                <div className='rounded-xl bg-amber-600 w-[30%] min-w-60 h-40'></div>
+            </section>
+
+            <section className='text-lg'>
+                About me
+            </section>
+
+            <section className='flex flex-col'>
+                <div className='text-3xl font-bold mb-7'>My expertise</div>
+                <div className='flex flex-wrap gap-2'>
+                    { skillsShuffle.map( (s) => createSkill(s) ) }
+                </div>
             </section>
         </div>
     )
