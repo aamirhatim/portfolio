48685b32618b3b89c637abc984392710e57e9d22
Aamir Husain <aamirhatim.h@gmail.com>
2025-06-23 22:38:06 -0400

    remove highlights section and make all projects uniform

--- src/components/pages/projectsPage.tsx
+++ src/components/pages/projectsPage.tsx
@@ -2,22 +2,10 @@ import projects from "../../data/projectData"
 import ProjectTile from "../atoms/ProjectTile/ProjectTile"
 
 export default function ProjectsPage() {
-    const numHighlights = 3
-
     return (
         <div className="flex flex-col gap-30">
-            {/* Highlights */}
-            <section className="box-border flex flex-wrap justify-center gap-5">
-                {projects.slice(0, numHighlights).map( (p, idx) => (
-                    <ProjectTile key={idx} project={p} highlight={true} />
-                ))}
-            </section>
-
-            <div className="text-3xl text-center">See more of my work on GitHub</div>
-
-            {/* Other projects */}
-            <section className="box-border flex flex-wrap justify-center gap-5">
-                {projects.slice(numHighlights + 1).map( (p, idx) => (
+            <section className="box-border flex flex-col gap-8">
+                {projects.map( (p, idx) => (
                     <ProjectTile key={idx} project={p} />
                 ))}
             </section>
