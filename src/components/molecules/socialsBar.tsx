d9cecacdc1cb4ee1090ac6a49304f9ecae65522b
Aamir Husain <aamirhatim.h@gmail.com>
2025-06-23 21:39:12 -0400

    add links

--- src/components/molecules/socialsBar.tsx
+++ src/components/molecules/socialsBar.tsx
@@ -4,11 +4,11 @@ import { faPencil } from '@fortawesome/free-solid-svg-icons'
 
 export default function SocialsBar() {
     return (
-        <div className="fixed right-5 bottom-5 box-border px-8 py-4 rounded-full backdrop-blur-md backdrop-brightness-70 flex justify-center gap-10">
-            <a href='https://github.com/aamirhatim' target='_blank'><FontAwesomeIcon icon={faGithubAlt} size='xl' color='white' /></a>
-            <a href='https://linkedin.com/in/aamirhatim' target='_blank'><FontAwesomeIcon icon={faLinkedinIn} size='xl' color='white' /></a>
-            <a href='https://instagram.com/aamirhatim' target='_blank'><FontAwesomeIcon icon={faInstagram} size='xl' color='white' /></a>
-            <a href='https://aamirhatim.wordpress.com' target='_blank'><FontAwesomeIcon icon={faPencil} size='xl' color='white' /></a>
+        <div className="flex justify-center gap-6">
+            <a href='https://github.com/aamirhatim' target='_blank'><FontAwesomeIcon icon={faGithubAlt} size='lg' color='white' /></a>
+            <a href='https://linkedin.com/in/aamirhatim' target='_blank'><FontAwesomeIcon icon={faLinkedinIn} size='lg' color='white' /></a>
+            <a href='https://instagram.com/aamirhatim' target='_blank'><FontAwesomeIcon icon={faInstagram} size='lg' color='white' /></a>
+            <a href='https://aamirhatim.wordpress.com' target='_blank'><FontAwesomeIcon icon={faPencil} size='lg' color='white' /></a>
         </div>
     )
 }
