539ef05a04ac13793097018fef4af0f993045926
Aamir Husain <aamirhatim.h@gmail.com>
2025-06-23 21:34:34 -0400

    implement a sidebar that reacts to nav selections

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,15 +1,22 @@
 import { createRootRoute, Outlet } from '@tanstack/react-router'
 import Navbar from '../components/molecules/navbar'
-import SocialsBar from '../components/molecules/socialsBar'
+import Sidebar from '../components/molecules/sidebar'
+import { useState } from 'react'
 
 export const Route = createRootRoute({
-    component: () => (
-        <div className='min-h-dvh w-dvw box-border flex flex-col'>
-            <Navbar />
-            <div className='box-border h-full pt-40 pb-15 px-[15%]'>
-                <Outlet />
+    component: () => {
+        const [nav, setNav] = useState<string>('')
+        const [sidebarViz, setSidebarViz] = useState<boolean>(false)
+
+        return (
+        <div className='h-dvh w-dvw box-border flex flex-col'>
+            <nav className='fixed top-0 left-0'><Navbar setNav={setNav} setSidebarViz={setSidebarViz}/></nav>
+            <div className='flex gap-20 h-full bg-amber-400'>
+                {sidebarViz && <Sidebar title={nav} setViz={setSidebarViz} /> }
+                <div className='box-border pr-[12%] py-40 grow overflow-y-scroll'>
+                    <Outlet />
+                </div>
             </div>
-            <SocialsBar />
         </div>
-    )
+    )}
 })
