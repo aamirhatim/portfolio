631e7c27d0dac79d536dc02f6eded6e6e44f4995
Aamir Husain <aamirhatim.h@gmail.com>
2025-06-23 21:48:35 -0400

    no wrap

--- src/components/molecules/patents.tsx
+++ src/components/molecules/patents.tsx
@@ -11,7 +11,7 @@ export default function Patents() {
     return (
         <section>
             <div className='text-4xl font-bold mb-6'>Patents</div>
-            <div className='flex flex-wrap gap-6 justify-center'>
+            <div className='flex flex-col gap-6'>
                 {patents.map( (p) => createPatentItem(p) )}
             </div>
         </section>
