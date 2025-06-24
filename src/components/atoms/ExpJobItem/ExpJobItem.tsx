2c5bee3f5fb7699423b37ac823348906ed32f557
Aamir Husain <aamirhatim.h@gmail.com>
2025-06-23 21:50:06 -0400

    make fonts smaller

--- src/components/atoms/ExpJobItem/ExpJobItem.tsx
+++ src/components/atoms/ExpJobItem/ExpJobItem.tsx
@@ -3,12 +3,12 @@ import { Job } from '../../../data/datatypes'
 export default function ExpJobItem(props: {job: Job}) {
     return (
         <div>
-            <div className='text-lg'>{props.job.start} - {props.job.end}</div>
-            <div className='text-2xl font-bold'>{props.job.title} <span>@{props.job.company}</span></div>
-            <div className='text-xl'>{props.job.description}</div>
+            <div className='text-md'>{props.job.start} - {props.job.end}</div>
+            <div className='text-xl font-bold'>{props.job.title} <span>@{props.job.company}</span></div>
+            <div className='text-lg'>{props.job.description}</div>
             {props.job.skills &&
                 <div className='flex flex-wrap gap-1 mt-2'>
-                    {props.job.skills.map( (s) => (<div className='box-border px-2 border rounded-full text-md'>{s}</div>))}
+                    {props.job.skills.map( (s) => (<div className='box-border px-2 border rounded-full text-xs'>{s}</div>))}
                 </div>
             }
         </div> 
