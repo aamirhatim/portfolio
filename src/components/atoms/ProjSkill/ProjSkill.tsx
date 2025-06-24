e281d20dd5bfa053a21c51d5f0e838eff18fa0a9
Aamir Husain <aamirhatim.h@gmail.com>
2025-06-23 22:38:30 -0400

    make font smaller

--- src/components/atoms/ProjSkill/ProjSkill.tsx
+++ src/components/atoms/ProjSkill/ProjSkill.tsx
@@ -1,5 +1,5 @@
 export default function ProjSkill(props: {value:string}) {
     return (
-        <div className='box-border border rounded-full px-3 text-sm'>{props.value}</div>
+        <div className='box-border border rounded-full px-3 text-xs'>{props.value}</div>
     )
 }
