934d7492bfcda50b30d7fb7c89f935d136a724cf
Aamir Husain <aamirhatim.h@gmail.com>
2025-06-23 21:48:18 -0400

    change size and layout

--- src/components/atoms/ExpPatentItem/ExpPatentItem.tsx
+++ src/components/atoms/ExpPatentItem/ExpPatentItem.tsx
@@ -2,10 +2,10 @@ import { Patent } from '../../../data/datatypes'
 
 function ExpPatentItem(props: {item: Patent}) {
     return (
-        <div className='box-border p-5 border rounded-xl flex flex-col justify-start items-center w-70 gap-4'>
-            <div className='box-border border rounded-full text-sm font-bold px-2'>{props.item.status}</div>
-            <div className='text-center text-xl font-bold'>{props.item.title}</div>
-            <div className='text-lg'>{props.item.description}</div>
+        <div className='box-border p-5 border rounded-xl flex flex-col justify-start w-[80%] min-w-100 gap-4'>
+            <div className='box-border border rounded-full text-xs font-bold px-2 w-min'>{props.item.status}</div>
+            <div className='text-lg font-bold'>{props.item.title}</div>
+            <div className='text-md'>{props.item.description}</div>
         </div> 
     )
 }
