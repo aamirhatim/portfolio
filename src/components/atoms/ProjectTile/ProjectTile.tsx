0bc0dd496f9acc4275184c475351a6e26db8d9ae
Aamir Husain <aamirhatim.h@gmail.com>
2025-06-23 22:39:33 -0400

    update layout and add an image placeholder

--- src/components/atoms/ProjectTile/ProjectTile.tsx
+++ src/components/atoms/ProjectTile/ProjectTile.tsx
@@ -2,7 +2,7 @@ import { ProjectType } from '../../../data/datatypes'
 import ProjLinkChip from '../ProjLinkChip/ProjLinkChip'
 import ProjSkill from '../ProjSkill/ProjSkill'
 
-function ProjectTile(props: {project:ProjectType, highlight?:boolean}) {
+function ProjectTile(props: {project:ProjectType}) {
 
     function createProjSkill(skill:string) {
         const key = 'proj-skill-'+skill.toLowerCase().replace(' ', '-')
@@ -11,19 +11,20 @@ function ProjectTile(props: {project:ProjectType, highlight?:boolean}) {
     }
 
     return (
-        <div className={`box-border border rounded-xl p-8 flex flex-col justify-between min-w-100 ${props.highlight ? ' h-70 w-full' : 'h-90 basis-[49%]'}`}>
-            <div>
-                <div className='proj-title-box'>
-                    <div className={`font-bold ${props.highlight ? 'text-3xl' : 'text-xl'}`}>{props.project.title}</div>
-                    {props.project.code && <ProjLinkChip value='Code' url={props.project.code} />}
-                    {props.project.video && <ProjLinkChip value='Video' url={props.project.video} />}
-                </div>
-                
-                <div className='text-lg mt-4'>{props.project.description}</div>
-            </div>
+        <div className={'box-border border rounded-xl p-4 flex gap-4'}>
+            <div className='h-50 min-w-60 rounded-xl bg-amber-800'></div>
 
-            <div className='flex flex-wrap gap-2'>
-                {props.project.skills.map( (s) => createProjSkill(s) )}
+            <div className='flex flex-col gap-2'>
+                <div className={'font-bold text-xl'}>{props.project.title}</div>
+
+                <div className='flex flex-wrap gap-1'>
+                    {props.project.skills.map( (s) => createProjSkill(s) )}
+                </div>
+
+                {props.project.code && <ProjLinkChip value='Code' url={props.project.code} />}
+                {props.project.video && <ProjLinkChip value='Video' url={props.project.video} />}
+                
+                <div className='text-md mt-4'>{props.project.description}</div>
             </div>
         </div>
     )
