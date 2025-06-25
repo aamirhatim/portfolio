85d8308570464406103da1229c7c29e6784c6a29
Aamir Husain <aamirhatim.h@gmail.com>
2025-06-24 20:35:25 -0400

    append code and video links to end of title

--- src/components/atoms/ProjectTile/ProjectTile.tsx
+++ src/components/atoms/ProjectTile/ProjectTile.tsx
@@ -12,17 +12,20 @@ function ProjectTile(props: {project:ProjectType}) {
 
     return (
         <div className={'box-border border rounded-xl p-4 flex gap-4'}>
-            <div className='h-50 min-w-60 rounded-xl bg-amber-800'></div>
+            <div className='box-border border h-50 min-w-60 rounded-xl'></div>
 
             <div className='flex flex-col gap-2'>
-                <div className={'font-bold text-xl'}>{props.project.title}</div>
+                
+
+                <div className='flex flex-wrap items-center gap-2'>
+                    <div className={'font-bold text-xl'}>{props.project.title}</div>
+                    {props.project.code && <ProjLinkChip value='{ Code }' url={props.project.code} />}
+                    {props.project.video && <ProjLinkChip value='{ Video }' url={props.project.video} />}
+                </div>
 
                 <div className='flex flex-wrap gap-1'>
                     {props.project.skills.map( (s) => createProjSkill(s) )}
                 </div>
-
-                {props.project.code && <ProjLinkChip value='Code' url={props.project.code} />}
-                {props.project.video && <ProjLinkChip value='Video' url={props.project.video} />}
                 
                 <div className='text-md mt-4'>{props.project.description}</div>
             </div>
