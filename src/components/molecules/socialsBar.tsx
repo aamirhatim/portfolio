0e53e2f2b8651deaac9d2397fe99771380efd77d
Aamir Husain <aamirhatim.h@gmail.com>
2025-07-05 20:14:19 -0400

    use a variable to set color

--- src/components/molecules/socialsBar.tsx
+++ src/components/molecules/socialsBar.tsx
@@ -3,12 +3,14 @@ import { faGithubAlt, faInstagram, faLinkedinIn } from '@fortawesome/free-brands
 import { faPencil } from '@fortawesome/free-solid-svg-icons'
 
 export default function SocialsBar() {
+    const iconColor = 'black'
+
     return (
         <div className="flex justify-center gap-6">
-            <a href='https://github.com/aamirhatim' target='_blank'><FontAwesomeIcon icon={faGithubAlt} size='lg' color='white' /></a>
-            <a href='https://linkedin.com/in/aamirhatim' target='_blank'><FontAwesomeIcon icon={faLinkedinIn} size='lg' color='white' /></a>
-            <a href='https://instagram.com/aamirhatim' target='_blank'><FontAwesomeIcon icon={faInstagram} size='lg' color='white' /></a>
-            <a href='https://aamirhatim.wordpress.com' target='_blank'><FontAwesomeIcon icon={faPencil} size='lg' color='white' /></a>
+            <a href='https://github.com/aamirhatim' target='_blank'><FontAwesomeIcon icon={faGithubAlt} size='lg' color={iconColor} /></a>
+            <a href='https://linkedin.com/in/aamirhatim' target='_blank'><FontAwesomeIcon icon={faLinkedinIn} size='lg' color={iconColor} /></a>
+            <a href='https://instagram.com/aamirhatim' target='_blank'><FontAwesomeIcon icon={faInstagram} size='lg' color={iconColor} /></a>
+            <a href='https://aamirhatim.wordpress.com' target='_blank'><FontAwesomeIcon icon={faPencil} size='lg' color={iconColor} /></a>
         </div>
     )
 }
