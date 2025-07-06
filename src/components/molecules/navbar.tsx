91d8cfc24438488d61ac3a974c4dea7f9c803d39
Aamir Husain <aamirhatim.h@gmail.com>
2025-07-05 20:13:57 -0400

    remove white color for text and add logic to highlight selected nav item

--- src/components/molecules/navbar.tsx
+++ src/components/molecules/navbar.tsx
@@ -18,14 +18,21 @@ export default function Navbar() {
 
     return (
         <div className='box-border w-dvw p-5'>
-            <div className='w-full box-border px-10 py-3 flex items-center gap-8 rounded-full text-l text-white backdrop-blur-md backdrop-brightness-99 shadow-[0_0_8px_rgba(0,0,0,0.1)]'>
+            <div className='w-full box-border px-10 py-3 flex items-center gap-8 rounded-full text-l backdrop-blur-md backdrop-brightness-99 shadow-[0_0_8px_rgba(0,0,0,0.1)]'>
                 <Link to='/' onClick={handleHomeClick}><img id='logo' className='size-[22px]' src={Logo} alt='logo' /></Link>
                 
                 <div className='flex grow justify-end gap-8 h-full'>
                     {
-                        navItems.map( (n, idx) => (
-                            <Link key={idx} to={n} className='content-center' onClick={() => handleNavClick(n)}>{n}</Link>
-                        ))
+                        navItems.map( (n, idx) => {
+                            let navColor = 'black'
+                            if (routeContext.nav === n) {
+                                navColor = 'white'
+                            }
+
+                            return (
+                                <Link key={idx} to={n} className={`content-center font-bold text-${navColor}`} onClick={() => handleNavClick(n)}>{n}</Link>
+                            )
+                        })
                     }
                 </div>
 
