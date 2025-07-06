68969e4272bec3f63cd3d0d801a731b7fe7c50e4
Aamir Husain <aamirhatim.h@gmail.com>
2025-07-06 10:32:59 -0400

    simplify nav color change logic. Add border

--- src/components/molecules/navbar.tsx
+++ src/components/molecules/navbar.tsx
@@ -18,21 +18,14 @@ export default function Navbar() {
 
     return (
         <div className='box-border w-dvw p-5'>
-            <div className='w-full box-border px-10 py-3 flex items-center gap-8 rounded-full text-l backdrop-blur-md backdrop-brightness-99 shadow-[0_0_8px_rgba(0,0,0,0.1)]'>
+            <div className='w-full box-border px-10 py-3 flex items-center gap-8 rounded-full text-l backdrop-blur-md backdrop-brightness-99 border-3 border-violet-950 shadow-[0_0_8px_rgba(0,0,0,0.2)]'>
                 <Link to='/' onClick={handleHomeClick}><img id='logo' className='size-[22px]' src={Logo} alt='logo' /></Link>
                 
                 <div className='flex grow justify-end gap-8 h-full'>
                     {
-                        navItems.map( (n, idx) => {
-                            let navColor = 'black'
-                            if (routeContext.nav === n) {
-                                navColor = 'white'
-                            }
-
-                            return (
-                                <Link key={idx} to={n} className={`content-center font-bold text-${navColor}`} onClick={() => handleNavClick(n)}>{n}</Link>
-                            )
-                        })
+                        navItems.map( (n, idx) => (
+                            <Link key={idx} to={n} className={`content-center font-bold ${routeContext.nav !== n && 'text-violet-500'}`} onClick={() => handleNavClick(n)}>{n}</Link>
+                        ))
                     }
                 </div>
 
