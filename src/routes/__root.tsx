ffc38ce2c30b26cca7714fe5f0c0c231e37e6fb8
Aamir Husain <aamirhatim.h@gmail.com>
2025-07-06 10:34:03 -0400

    move styling to parent

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -9,9 +9,9 @@ export const Route = createRootRouteWithContext<RouterContext>()({
         const routeContext = Route.useRouteContext()
 
         return (
-        <div className='h-dvh w-dvw box-border flex flex-col'>
+        <div className='h-dvh w-dvw box-border flex flex-col bg-violet-950 text-violet-100'>
             <nav className='fixed top-0 left-0'><Navbar /></nav>
-            <div className='flex gap-20 h-full bg-amber-400'>
+            <div className='flex gap-20 h-full'>
                 {routeContext.nav != '' && <Sidebar title={routeContext.nav} /> }
                 <div className='box-border pr-[12%] py-40 grow overflow-y-scroll'>
                     <Outlet />
