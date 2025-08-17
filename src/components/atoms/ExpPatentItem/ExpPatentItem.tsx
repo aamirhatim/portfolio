cdd0d7879bfc24806188d48e0e777a05c5950ca3
Aamir Husain <aamirhatim.h@gmail.com>
2025-08-17 17:27:31 -0400

    experiment with color

--- src/components/atoms/ExpPatentItem/ExpPatentItem.tsx
+++ src/components/atoms/ExpPatentItem/ExpPatentItem.tsx
@@ -2,7 +2,7 @@ import { Patent } from '../../../data/datatypes'
 
 function ExpPatentItem(props: {item: Patent}) {
     return (
-        <div className='box-border p-5 border rounded-xl flex flex-col justify-start w-[80%] min-w-100 gap-4'>
+        <div className='box-border p-5 border border-violet-700 rounded-xl flex flex-col justify-start w-[80%] min-w-100 gap-4'>
             <div className='flex gap-2 items-center'>
                 <div className='box-border border rounded-full text-xs font-bold px-2 w-min'>{props.item.status}</div>
                 {props.item.status === "GRANTED" && 
@@ -11,7 +11,7 @@ function ExpPatentItem(props: {item: Patent}) {
                     </a>
                 }
             </div>
-            <div className='text-lg font-bold'>{props.item.title}</div>
+            <div className='text-lg font-bold text-blue-400'>{props.item.title}</div>
             <div className='text-md'>{props.item.description}</div>
         </div> 
     )
