3ac31c71a2ced83b3a5884b1177dadbe0084f81a
Aamir Husain <aamirhatim.h@gmail.com>
2025-08-17 17:26:19 -0400

    play with colors

--- src/components/atoms/ExpJobItem/ExpJobItem.tsx
+++ src/components/atoms/ExpJobItem/ExpJobItem.tsx
@@ -3,12 +3,12 @@ import { Job } from '../../../data/datatypes'
 export default function ExpJobItem(props: {job: Job}) {
     return (
         <div>
-            <div className='text-md'>{props.job.start} - {props.job.end}</div>
-            <div className='text-xl font-bold'>{props.job.title} <span>@{props.job.company}</span></div>
+            <div className='text-md text-violet-400'>{props.job.start} - {props.job.end}</div>
+            <div className='text-xl font-bold text-blue-400'>{props.job.title} <span className='text-orange-500'>@{props.job.company}</span></div>
             <div className='text-lg'>{props.job.description}</div>
             {props.job.skills &&
-                <div className='flex flex-wrap gap-1 mt-2'>
-                    {props.job.skills.map( (s, idx) => (<div key={idx} className='box-border px-2 border rounded-full text-xs'>{s}</div>))}
+                <div className='flex flex-wrap gap-1 mt-4'>
+                    {props.job.skills.map( (s, idx) => (<div key={idx} className='box-border px-2 border border-violet-400 text-violet-400 rounded-full text-xs'>{s}</div>))}
                 </div>
             }
         </div> 
