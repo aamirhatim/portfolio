a5f6109db6e411c6d638d4e394d2233cac144b27
Aamir Husain <aamirhatim.h@gmail.com>
2025-08-17 17:31:21 -0400

    experiment with colors

--- src/components/molecules/currentWork.tsx
+++ src/components/molecules/currentWork.tsx
@@ -2,10 +2,15 @@ import { currentWorkDesc, jobs } from "../../data/jobs";
 
 export default function CurrentWork() {
     return (
-        <section className='box-border border rounded-xl p-10'>
-            <div className='text-lg'>{jobs[0].start} - Present</div>
-            <div className='text-3xl font-bold'>{jobs[0].title} <span>@{jobs[0].company}</span></div>
+        <section className='box-border border border-violet-700 rounded-xl p-10'>
+            <div className='text-lg text-violet-400'>{jobs[0].start} - Present</div>
+            <div className='text-3xl font-bold text-blue-400'>{jobs[0].title} <span className='text-red-500'>@{jobs[0].company}</span></div>
             <div className='box-border mt-4 text-xl'>{currentWorkDesc}</div>
+            {jobs[0].skills &&
+                <div className='flex flex-wrap gap-1 mt-8'>
+                    {jobs[0].skills.map( (s, idx) => (<div key={idx} className='box-border px-2 border border-violet-400 text-violet-400 rounded-full text-xs'>{s}</div>))}
+                </div>
+            }
         </section>
     )
 }
