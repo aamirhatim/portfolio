76334a26de7bd23f71da685e46737764efde0c56
Aamir Husain <aamirhatim.h@gmail.com>
2025-08-17 17:26:46 -0400

    add different highlight color

--- src/components/atoms/ProjLinkChip/ProjLinkChip.tsx
+++ src/components/atoms/ProjLinkChip/ProjLinkChip.tsx
@@ -1,5 +1,5 @@
 export default function ProjLinkChip(props: {value:string, url:string}) {
     return (
-        <div className='text-sm font-bold'><a href={props.url} target='_blank'>{props.value}</a></div>
+        <div className='text-sm font-bold text-lime-500'><a href={props.url} target='_blank'>{props.value}</a></div>
     )
 }
