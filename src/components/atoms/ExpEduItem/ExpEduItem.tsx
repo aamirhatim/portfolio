5eed4ffb00e1f60245e4055b827f9159fd151d49
Aamir Husain <aamirhatim.h@gmail.com>
2025-08-17 17:25:29 -0400

    experiment with colors

--- src/components/atoms/ExpEduItem/ExpEduItem.tsx
+++ src/components/atoms/ExpEduItem/ExpEduItem.tsx
@@ -3,8 +3,8 @@ import { Education } from '../../../data/datatypes'
 export default function ExpEduItem(props: {item:Education}){
     return (
         <div>
-            <div className='text-xl font-bold'>{props.item.degree}: <span>{props.item.major}</span></div>
-            <div className='text-lg'>{props.item.school} - {props.item.yearEnd}</div>
+            <div className='text-lg text-violet-400'>{props.item.school} - {props.item.yearEnd}</div>
+            <div className='text-xl font-bold text-blue-400'>{props.item.degree}: <span className='text-red-500'>{props.item.major}</span></div>
         </div>
     )
 }
