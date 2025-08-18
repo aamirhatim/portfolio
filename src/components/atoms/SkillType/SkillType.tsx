23e2115c96d4525c1ff6d38387a3067ee8f89c4d
Aamir Husain <aamirhatim.h@gmail.com>
2025-08-17 21:36:32 -0400

    remove old and unused components

--- src/components/atoms/SkillType/SkillType.tsx
+++
@@ -1,8 +0,0 @@
-export default function SkillType(props: {title: string, description: string}) {
-    return (
-        <div className='box-border w-80 border rounded-xl p-10'>
-            <div className={'mb-4 font-bold text-3xl'}>{props.title}</div>
-            <div className='text-xl'>{props.description}</div>
-        </div>
-    )
-}
