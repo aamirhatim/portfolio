084c6e12dbfe13c8175024583df385116ab3a1c6
Aamir Husain <aamirhatim.h@gmail.com>
2025-09-10 22:51:03 -0400

    remove extra handler

--- src/components/molecules/schooling.tsx
+++ src/components/molecules/schooling.tsx
@@ -1,18 +1,12 @@
 import { educationList } from '../../data/education'
-import { EducationType } from '../../data/datatypes'
 import ExpEduItem from '../atoms/ExpEduItem'
-
-function createEduItem(edu:EducationType) {
-        const key = 'edu-' + edu.id
-        return <ExpEduItem key={key} item={edu} />
-    }
 
 export default function Schooling() {
     return (
         <section>
             <div className='text-4xl font-bold mb-6'>education.</div>
             <div className='flex flex-col gap-6'>
-                {educationList.map( (edu) => createEduItem(edu) )}
+                {educationList.map( (edu, idx) => <ExpEduItem key={idx} item={edu} /> )}
             </div>
         </section>
     )
