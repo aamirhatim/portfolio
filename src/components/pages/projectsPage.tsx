03b539863a0ac63918c7e582b804689b5a474fea
Aamir Husain <aamirhatim.h@gmail.com>
2025-09-10 22:52:49 -0400

    clean up

--- src/components/pages/projectsPage.tsx
+++ src/components/pages/projectsPage.tsx
@@ -1,13 +1,11 @@
 import projects from "../../data/projectData"
-import ProjectTile from "../atoms/ProjectTile/ProjectTile"
+import ProjectItem from "../atoms/ProjectItem"
 
 export default function ProjectsPage() {
     return (
         <div className="flex flex-col gap-30">
             <section className="box-border flex flex-col gap-8">
-                {projects.map( (p, idx) => (
-                    <ProjectTile key={idx} project={p} />
-                ))}
+                {projects.map( (p, idx) => <ProjectItem key={idx} project={p} /> )}
             </section>
         </div>
     )
