aa580103337b9795f2f479039dac5325e0f1445b
Aamir Husain <aamirhatim.h@gmail.com>
2025-09-10 22:51:46 -0400

    Use color vars in tailwind classes

--- src/components/atoms/ExpEduItem.tsx
+++ src/components/atoms/ExpEduItem.tsx
@@ -3,8 +3,8 @@ import { EducationType } from '../../data/datatypes'
 export default function ExpEduItem(props: {item:EducationType}){
     return (
         <div>
-            <div className='text-lg text-violet-400'>{props.item.school} - {props.item.yearEnd}</div>
-            <div className='text-xl font-bold text-blue-400'>{props.item.degree}: <span className='text-red-500'>{props.item.major}</span></div>
+            <div className='text-lg text-[var(--txt-subtitle-color)]'>{props.item.school} - {props.item.yearEnd}</div>
+            <div className='text-xl font-bold text-[var(--txt-title-color)]'>{props.item.degree}: <span className='text-[var(--txt-accent-color)]'>{props.item.major}</span></div>
         </div>
     )
 }
