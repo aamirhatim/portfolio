58d941bf21a13e08cd15b59f10a3af4260466f17
Aamir Husain <aamirhatim.h@gmail.com>
2025-09-10 22:54:50 -0400

    use tailwind classes and color variables

--- src/components/atoms/ExpPatentItem.tsx
+++ src/components/atoms/ExpPatentItem.tsx
@@ -2,16 +2,16 @@ import { PatentType } from '../../data/datatypes'
 
 export default function ExpPatentItem(props: {item: PatentType}) {
     return (
-        <div className='box-border p-5 border border-violet-700 rounded-xl flex flex-col justify-start w-[80%] min-w-100 gap-4'>
+        <div className='box-border p-5 border border-[var(--border-color)] rounded-xl flex flex-col justify-start w-[80%] min-w-100 gap-4'>
             <div className='flex gap-2 items-center'>
                 <div className='box-border border rounded-full text-xs font-bold px-2 w-min'>{props.item.status}</div>
                 {props.item.status === "GRANTED" && 
-                    <a href={props.item.url} target='_blank'>
-                        <div className='text-sm font-bold underline'>US Patent No. {props.item.number}</div>
-                    </a>
+                <a href={props.item.url} target='_blank'>
+                    <div className='text-sm font-bold underline'>US Patent No. {props.item.number}</div>
+                </a>
                 }
             </div>
-            <div className='text-lg font-bold text-blue-400'>{props.item.title}</div>
+            <div className='text-lg font-bold text-[var(--txt-title-color)]'>{props.item.title}</div>
             <div className='text-md'>{props.item.description}</div>
         </div> 
     )
