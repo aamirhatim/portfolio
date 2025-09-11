66f793ef30e17922c2d4de6ea27f27c71652d3ce
Aamir Husain <aamirhatim.h@gmail.com>
2025-09-10 22:45:14 -0400

    Use tailwind classes

--- src/components/atoms/ExpJobItem.tsx
+++ src/components/atoms/ExpJobItem.tsx
@@ -1,15 +1,16 @@
 import { JobType } from '../../data/datatypes'
+import ChipGroup from '../molecules/ChipGroup'
 
 export default function ExpJobItem(props: {job: JobType}) {
     return (
         <div>
-            <div className='text-md text-violet-400'>{props.job.start} - {props.job.end}</div>
-            <div className='text-xl font-bold text-blue-400'>{props.job.title} <span className='text-orange-500'>@{props.job.company}</span></div>
+            <div className='text-md text-[var(--txt-subtitle-color)]'>{props.job.start} - {props.job.end}</div>
+            <div className='text-xl font-bold text-[var(--txt-title-color)]'>{props.job.title} <span className='text-[var(--txt-accent-color)]'>@{props.job.company}</span></div>
             <div className='text-lg'>{props.job.description}</div>
             {props.job.skills &&
-                <div className='flex flex-wrap gap-1 mt-4'>
-                    {props.job.skills.map( (s, idx) => (<div key={idx} className='box-border px-2 border border-violet-400 text-violet-400 rounded-full text-xs'>{s}</div>))}
-                </div>
+            <div className='mt-3'>
+                <ChipGroup list={props.job.skills} />
+            </div>
             }
         </div> 
     )
