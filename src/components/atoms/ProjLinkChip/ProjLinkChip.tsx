b393b257c003e53da24303f5e219d64f79f9cf40
Aamir Husain <aamirhatim.h@gmail.com>
2025-09-10 23:13:00 -0400

    remove old files

--- src/components/atoms/ProjLinkChip/ProjLinkChip.tsx
+++
@@ -1,5 +0,0 @@
-export default function ProjLinkChip(props: {value:string, url:string}) {
-    return (
-        <div className='text-sm font-bold text-lime-500'><a href={props.url} target='_blank'>{props.value}</a></div>
-    )
-}
--- src/components/atoms/ProjSkill.tsx
+++
@@ -1,5 +0,0 @@
-export default function ProjSkill(props: {value:string}) {
-    return (
-        <div className='box-border border rounded-full px-3 text-xs'>{props.value}</div>
-    )
-}
