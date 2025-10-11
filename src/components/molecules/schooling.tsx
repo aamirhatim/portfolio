5db8f6ac8b858f7e93044806de56eb189964a628
Aamir Husain <aamirhatim.h@gmail.com>
2025-10-11 16:43:33 -0400

    use Firestore to get edu list

--- src/components/molecules/schooling.tsx
+++ src/components/molecules/schooling.tsx
@@ -1,12 +1,30 @@
-import { educationList } from '../../data/education'
+import { useEffect, useState } from 'react';
+import { useFirebaseAppContext } from '../../context/firebaseAppContext'
 import ExpEduItem from '../atoms/ExpEduItem'
+import { EducationType, FirestoreDocType } from '../../data/datatypes';
+import { getAllDocumentsFromCollection } from '../../lib/firestoreLib';
 
 export default function Schooling() {
+    // Get context
+    const firebaseAppContext = useFirebaseAppContext();
+
+    // Init state
+    const [eduList, setEduList] = useState<FirestoreDocType[]>([]);
+
+    // Get list of education
+    useEffect( () => {
+        const getEdu = async () => {
+            const edu = await getAllDocumentsFromCollection(firebaseAppContext, "education");
+            setEduList(edu);
+        };
+        getEdu();
+    }, []);
+
     return (
         <section>
             <div className='text-4xl font-bold mb-6'>education.</div>
             <div className='flex flex-col gap-6'>
-                {educationList.map( (edu, idx) => <ExpEduItem key={idx} item={edu} /> )}
+                {eduList.map( (edu, idx) => <ExpEduItem key={idx} item={edu.data as EducationType} /> )}
             </div>
         </section>
     )
