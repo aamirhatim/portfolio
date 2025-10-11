e2202f412fda0b07d7d6943b9669758f56f1b3b0
Aamir Husain <aamirhatim.h@gmail.com>
2025-10-11 15:48:21 -0400

    Create generic helper to get all documents from  collection

---
+++ src/lib/firestoreLib.tsx
@@ -0,0 +1,19 @@
+import { FirebaseApp } from "firebase/app";
+import { collection, getDocs, getFirestore } from "firebase/firestore";
+import { FirestoreDocType } from "../data/datatypes";
+
+/**
+ * Returns list of documents from a given Firestore Collection
+ * @param firebaseApp - Firebase app object
+ * @param collectionName - Collection name
+ * @returns List of Firestore documents
+ */
+export async function getItemsFromCollection(firebaseApp:FirebaseApp, collectionName:string): Promise<FirestoreDocType[]> {
+    const db = getFirestore(firebaseApp);
+    const querySnapshot = await getDocs(collection(db, collectionName));
+    const items:FirestoreDocType[] = querySnapshot.docs.map((doc) => ({
+        id: doc.id,
+        data: doc.data(),
+    }));
+    return items;
+}
