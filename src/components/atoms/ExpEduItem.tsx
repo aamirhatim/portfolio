412fa9a5f528dc1cfb2dad766b5e210900f50e0e
Aamir Husain <aamirhatim.h@gmail.com>
2025-10-11 16:43:49 -0400

    update to match new type structure

--- src/components/atoms/ExpEduItem.tsx
+++ src/components/atoms/ExpEduItem.tsx
@@ -4,7 +4,7 @@ export default function ExpEduItem(props: {item:EducationType}){
     return (
         <div>
             <div className='text-lg text-[var(--txt-subtitle-color)]'>{props.item.school} - {props.item.yearEnd}</div>
-            <div className='text-xl font-bold text-[var(--txt-title-color)]'>{props.item.degree}: <span className='text-[var(--txt-accent-color)]'>{props.item.major}</span></div>
+            <div className='text-xl font-bold text-[var(--txt-title-color)]'>{props.item.degree.long}: <span className='text-[var(--txt-accent-color)]'>{props.item.field}</span></div>
         </div>
     )
 }
