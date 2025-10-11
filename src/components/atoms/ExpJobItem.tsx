28eb669cd3701693c9ffd4d742b622e1b47a5d12
Aamir Husain <aamirhatim.h@gmail.com>
2025-10-11 17:03:12 -0400

    update to match new type structure

--- src/components/atoms/ExpJobItem.tsx
+++ src/components/atoms/ExpJobItem.tsx
@@ -4,13 +4,13 @@ import ChipGroup from '../molecules/ChipGroup'
 export default function ExpJobItem(props: {job: JobType}) {
     return (
         <div>
-            <div className='text-md text-[var(--txt-subtitle-color)]'>{props.job.start} - {props.job.end}</div>
+            <div className='text-md text-[var(--txt-subtitle-color)]'>{props.job.startDate} - {props.job.endDate}</div>
             <div className='text-xl font-bold text-[var(--txt-title-color)]'>{props.job.title} <span className='text-[var(--txt-accent-color)]'>@{props.job.company}</span></div>
             <div className='text-lg'>{props.job.description}</div>
             {props.job.skills &&
-            <div className='mt-3'>
-                <ChipGroup list={props.job.skills} />
-            </div>
+                <div className='mt-3'>
+                    <ChipGroup list={props.job.skills} />
+                </div>
             }
         </div> 
     )
