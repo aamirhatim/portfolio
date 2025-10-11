11efb76023c9aec9925efe6d815c08bbdf785b39
Aamir Husain <aamirhatim.h@gmail.com>
2025-10-11 16:34:54 -0400

    Update logic for patent status

--- src/components/atoms/ExpPatentItem.tsx
+++ src/components/atoms/ExpPatentItem.tsx
@@ -4,8 +4,8 @@ export default function ExpPatentItem(props: {item: PatentType}) {
     return (
         <div className='box-border p-5 border border-[var(--border-color)] rounded-xl flex flex-col justify-start w-[80%] min-w-100 gap-4'>
             <div className='flex gap-2 items-center'>
-                <div className='box-border border rounded-full text-xs font-bold px-2 w-min'>{props.item.status}</div>
-                {props.item.status === "GRANTED" && 
+                <div className='box-border border rounded-full text-xs font-bold px-2 w-min'>{props.item.status.toUpperCase()}</div>
+                {props.item.status === "granted" && 
                 <a href={props.item.url} target='_blank'>
                     <div className='text-sm font-bold underline'>US Patent No. {props.item.number}</div>
                 </a>
