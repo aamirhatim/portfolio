77ab7affb4e3d106787cdba3e39bb78a3b0a9ed4
Aamir Husain <aamirhatim.h@gmail.com>
2025-10-12 21:15:03 -0400

    add option for large chip group

--- src/components/molecules/ChipGroup.tsx
+++ src/components/molecules/ChipGroup.tsx
@@ -1,9 +1,16 @@
 import Chip from "../atoms/Chip"
 
-export default function ChipGroup(props: {list:Array<string>}) {
+export interface ChipGroupProps {
+    list: string[],
+    size?: string,
+}
+
+export default function ChipGroup(props:ChipGroupProps) {
+    const gapSize = props.size === "lg" ? "gap-3" : "gap-2";
+
     return (
-        <div className="flex flex-wrap gap-2">
-            {props.list.map( (s, idx) => <Chip key={idx} text={s} /> )}
+        <div className={`flex flex-wrap ${gapSize}`}>
+            {props.list.map( (s, idx) => <Chip key={idx} text={s} size={props.size} /> )}
         </div>
     )
 }
