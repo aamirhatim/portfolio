f637d701723eec651b502c0cb87e26f8e2ddca4c
Aamir Husain <aamirhatim.h@gmail.com>
2025-10-12 21:17:30 -0400

    use Firestore for grabbing skill list. Add useEffect for categorizing them

--- src/components/pages/AboutPage.tsx
+++ src/components/pages/AboutPage.tsx
@@ -1,19 +1,45 @@
-import { shuffle } from 'lodash'
-import { skillSet } from '../../data/skillsData'
 import { SkillType } from "../../data/datatypes"
-import SkillItem from "../atoms/SkillItem"
-
-function createSkill(skill: SkillType) {
-    const key = 'skill-' + skill.value.toLowerCase().replace(' ', '-')
-
-    return(
-        <SkillItem key={key} value={skill.value} type={skill.type} />
-    )
-}
+import { useFirebaseAppContext } from '../../context/firebaseAppContext'
+import { useEffect, useState } from 'react'
+import { getAllDocumentsFromCollection } from '../../lib/firestoreLib'
+import ChipGroup from "../molecules/ChipGroup"
 
 export default function AboutPage() {
-    // Shuffle skills
-    const skillsShuffle = shuffle(skillSet)
+    // Get context
+    const firebaseAppContext = useFirebaseAppContext();
+
+    // Init state
+    const [topSkills, setTopSkills] = useState<SkillType[]>([]);
+    const [midSkills, setMidSkills] = useState<SkillType[]>([]);
+    const [lowSkills, setLowSkills] = useState<SkillType[]>([]);
+
+    // Get skills
+    useEffect( () => {
+        const getSkills = async () => {
+            const skills = await getAllDocumentsFromCollection(firebaseAppContext, "skills");
+
+            // Categorize skills
+            const tops:SkillType[] = [];
+            const mids:SkillType[] = [];
+            const lows:SkillType[] = [];
+            skills.map((skill) => {
+                const data = skill.data as SkillType;
+                if (data.level >= 4) {
+                    tops.push(data);
+                } else if (data.level >= 2) {
+                    mids.push(data);
+                } else {
+                    lows.push(data);
+                }
+            });
+
+            // Update lists
+            setTopSkills(tops);
+            setMidSkills(mids);
+            setLowSkills(lows);
+        };
+        getSkills();
+    }, []);
 
     return (
         <div className="flex flex-col gap-20">
@@ -26,9 +52,19 @@ export default function AboutPage() {
             </section>
 
             <section className='flex flex-col'>
-                <div className='text-3xl font-bold mb-7'>my expertise.</div>
-                <div className='flex flex-wrap gap-2'>
-                    { skillsShuffle.map( (s) => createSkill(s) ) }
+                <div className="mb-10">
+                    <div className='text-3xl font-bold mb-5'>I'm pretty good at:</div>
+                    <ChipGroup list={topSkills.map(i => i.name)} size="lg" />
+                </div>
+
+                <div className="mb-10">
+                    <div className='text-3xl font-bold mb-5'>I'm familiar with:</div>
+                    <ChipGroup list={midSkills.map(i => i.name)} size="lg" />
+                </div>
+
+                <div className="mb-10">
+                    <div className='text-3xl font-bold mb-5'>I've dabbled in:</div>
+                    <ChipGroup list={lowSkills.map(i => i.name)} size="lg" />
                 </div>
             </section>
         </div>
