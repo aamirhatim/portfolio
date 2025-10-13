4c5f7682feae3411e6a20cfa5c502ce161e93888
Aamir Husain <aamirhatim.h@gmail.com>
2025-10-12 21:09:35 -0400

    Add option for large chips

--- src/components/atoms/Chip.tsx
+++ src/components/atoms/Chip.tsx
@@ -1,7 +1,24 @@
-export default function Chip(props: {text:string}) {
-    return (
+export interface ChipProps {
+    text: string,
+    size?: string,
+}
+
+export default function Chip(props:ChipProps) {
+    const smallChip = (
         <div className="box-border px-2 border border-[var(--txt-subtitle-color)] text-[var(--txt-subtitle-color)] rounded-full text-xs">
             {props.text}
         </div>
+    )
+
+    const largeChip = (
+        <div className="box-border px-2 border border-[var(--txt-subtitle-color)] text-[var(--txt-subtitle-color)] rounded-full text-xl">
+            {props.text}
+        </div>
+    )
+
+    return (
+        <>
+            {props.size === "lg" ? largeChip : smallChip}
+        </>
     )
 }
