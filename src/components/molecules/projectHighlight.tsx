86c3487b3dc6e67008376352b152990441b88f22
Aamir Husain <aamirhatim.h@gmail.com>
2025-10-12 21:26:49 -0400

    update color palette for proj highlight component

--- src/components/molecules/projectHighlight.tsx
+++ src/components/molecules/projectHighlight.tsx
@@ -1,11 +1,26 @@
+import { useEffect, useRef } from "react";
 import { ProjectType } from "../../data/datatypes"
 
 export default function ProjectHighlight(props: {project:ProjectType}) {
+    // Create refs
+    const imgRef = useRef<HTMLDivElement>(null);
+
+    // Set project bg image
+    useEffect( () => {
+        if (!imgRef.current || !props.project.img) return;
+
+        const img = imgRef.current;
+        const imgPath = `/project_img/${props.project.img}`;
+        img.style.backgroundImage = `url(${imgPath})`;
+        img.style.backgroundSize = "cover";
+        img.style.backgroundPosition = "center";
+    }, []);
+    
     return (
         <div className="flex">
-            <div className="box-border border rounded-md h-30 w-40"></div>
+            <div ref={imgRef} className="box-border border border-[var(--border-color)] rounded-xl h-30 w-40"></div>
             <div className="box-border px-4 w-80">
-                <div className="text-md font-bold mb-2">{props.project.title}</div>
+                <div className="text-md font-bold text-[var(--txt-title-color)] mb-2">{props.project.title}</div>
                 <div className="text-sm">{props.project.description}</div>
             </div>
         </div>
