f7323223d195029d654b877fac47311ae2f6cabe
Aamir Husain <aamirhatim.h@gmail.com>
2025-10-20 19:34:22 -0400

    remove colons

--- src/components/pages/AboutPage.tsx
+++ src/components/pages/AboutPage.tsx
@@ -53,17 +53,17 @@ export default function AboutPage() {
 
             <section className='flex flex-col'>
                 <div className="mb-10">
-                    <div className='text-3xl font-bold mb-5'>I'm pretty good at:</div>
+                    <div className='text-3xl font-bold mb-5'>I'm pretty good at</div>
                     <ChipGroup list={topSkills.map(i => i.name)} size="lg" />
                 </div>
 
                 <div className="mb-10">
-                    <div className='text-3xl font-bold mb-5'>I'm familiar with:</div>
+                    <div className='text-3xl font-bold mb-5'>I'm familiar with</div>
                     <ChipGroup list={midSkills.map(i => i.name)} size="lg" />
                 </div>
 
                 <div className="mb-10">
-                    <div className='text-3xl font-bold mb-5'>I've dabbled in:</div>
+                    <div className='text-3xl font-bold mb-5'>I've dabbled in</div>
                     <ChipGroup list={lowSkills.map(i => i.name)} size="lg" />
                 </div>
             </section>
