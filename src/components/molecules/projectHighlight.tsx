00cbf7e3f5961c6e0e754bdfd898714c34a1d198
Aamir Husain <aamirhatim.h@gmail.com>
2025-10-23 18:37:47 -0400

    update to use lazyimg component

--- src/components/molecules/projectHighlight.tsx
+++ src/components/molecules/projectHighlight.tsx
@@ -1,24 +1,18 @@
-import { useEffect, useRef } from "react";
 import { ProjectType } from "../../data/datatypes"
+import LazyImg from "../atoms/LazyImg";
 
 export default function ProjectHighlight(props: {project:ProjectType}) {
-    // Create refs
-    const imgRef = useRef<HTMLDivElement>(null);
-
-    // Set project bg image
-    useEffect( () => {
-        if (!imgRef.current || !props.project.img) return;
-
-        const img = imgRef.current;
-        const imgPath = `/project_img/${props.project.img}`;
-        img.style.backgroundImage = `url(${imgPath})`;
-        img.style.backgroundSize = "cover";
-        img.style.backgroundPosition = "center";
-    }, []);
+    const imgPath = `/proj_img/${props.project.imgPath}`;
+    const placeholderPath = `/proj_thumbs/${props.project.imgPath}`;
     
     return (
         <div className="flex">
-            <div ref={imgRef} className="box-border border border-[var(--border-color)] rounded-xl h-30 w-40"></div>
+            <LazyImg
+                imgPath={imgPath}
+                alt="Project Image"
+                className="box-border border border-[var(--border-color)] rounded-xl h-30 w-40 grow-0 shrink-0"
+                placeholderPath={placeholderPath}                
+            />
             <div className="box-border px-4 w-80">
                 <div className="text-md font-bold text-[var(--txt-title-color)] mb-2">{props.project.title}</div>
                 <div className="text-sm">{props.project.description}</div>
