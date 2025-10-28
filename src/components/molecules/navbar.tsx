bc1ac603abc5f9ff4f42d7a077f73488c0b2b3bb
Aamir Husain <aamirhatim.h@gmail.com>
2025-10-28 18:46:59 -0400

    remove border around navbar

--- src/components/molecules/navbar.tsx
+++ src/components/molecules/navbar.tsx
@@ -23,7 +23,7 @@ export default function Navbar() {
 
     return (
         <nav id="navbar" className='box-border w-dvw fixed top-0 left-0 flex justify-center p-5 z-10'>
-            <div className='w-full w-max-view box-border px-10 py-3 flex items-center gap-8 rounded-full text-l backdrop-blur-md backdrop-brightness-99 border-3 border-violet-950 shadow-[0_0_8px_rgba(0,0,0,0.2)]'>
+            <div className='w-full w-max-view box-border px-10 py-3 flex items-center gap-8 rounded-full text-l backdrop-blur-md backdrop-brightness-70 shadow-[0_0_8px_rgba(0,0,0,0.2)]'>
                 <Link to='/' onClick={handleHomeClick}><img id='logo' className='size-[22px]' src={Logo} alt='logo' /></Link>
                 
                 <div className='flex grow justify-start gap-8 h-full'>
