2e9da4981bbb05428f8577575073bfb65b006574
Aamir Husain <aamirhatim.h@gmail.com>
2025-11-08 00:59:20 -0500

    Create dropdown menu component for nav on mobile. Add mobile styling for nav bar

---
+++ src/components/molecules/NavMenu.tsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import { useAppContext } from "../../context/appContext";
+import SocialsBar from "./socialsBar";
+
+const navItems:string[] = [
+    "home",
+    "about",
+    "resume",
+    "projects"
+]
+
+export default function NavMenu() {
+    // Get context
+    const appContext = useAppContext();
+
+    // Init state
+    const [navMenuVis, setNavMenuVis] = useState<boolean>(false);
+
+    const handleClick = (e:any) => {
+        e.preventDefault();
+
+        appContext.setNavSelect(e.target.innerText);
+        setNavMenuVis(false);
+    };
+    
+    return (
+        <div className="relative flex flex-col items-end">
+            <div className="font-bold text-[var(--txt-accent-color)]" onClick={() => setNavMenuVis(!navMenuVis)}>{appContext.navSelect}</div>
+
+            {navMenuVis &&
+                <div className="absolute top-full right-0 w-max mt-5 p-6 flex flex-col items-center gap-4 rounded-xl bg-[var(--bg-color)] shadow-2xl">
+                    {navItems.map((nav, idx) => {
+                        return (
+                            <div key={idx} className="p-2" onClick={handleClick}>{nav}</div>
+                        )
+                    })}
+
+                    <div className="mt-4">
+                        <SocialsBar />
+                    </div>
+                </div>
+            }
+        </div>
+    );
+}
--- src/components/molecules/navbar.tsx
+++ src/components/molecules/navbar.tsx
@@ -1,5 +1,7 @@
 import Logo from '../../assets/aamirhatim_logo.svg'
 import { useAppContext } from '../../context/appContext'
+import useIsMobile from '../hooks'
+import NavMenu from './NavMenu'
 import SocialsBar from './socialsBar'
 
 const navItems:Array<string> = [
@@ -11,6 +13,7 @@ const navItems:Array<string> = [
 export default function Navbar() {
     // Get context
     const appContext = useAppContext()
+    const isMobiile = useIsMobile();
 
     const handleHomeClick = () => {
         appContext.setNavSelect("home")
@@ -20,8 +23,8 @@ export default function Navbar() {
         appContext.setNavSelect(title)
     }
 
-    return (
-        <nav id="navbar" className='box-border w-dvw fixed top-0 left-0 flex justify-center p-5 z-10'>
+    const desktopLayout = (
+        <nav className='box-border w-dvw fixed top-0 left-0 flex justify-center p-5 z-10'>
             <div className='w-full w-max-view box-border px-10 py-3 flex items-center gap-8 rounded-full text-l backdrop-blur-md backdrop-brightness-70 shadow-[0_0_8px_rgba(0,0,0,0.2)]'>
                 <div onClick={handleHomeClick}><img id='logo' className='size-[22px]' src={Logo} alt='logo' /></div>
                 
@@ -36,5 +39,20 @@ export default function Navbar() {
                 <SocialsBar />
             </div>
         </nav>
+    );
+
+    const mobileLayout = (
+        <nav className='box-border w-dvw fixed top-0 left-0 flex justify-center p-5 z-10'>
+            <div className='w-full w-max-view box-border px-8 py-3 h-12 flex items-center justify-between gap-8 rounded-full backdrop-blur-md backdrop-brightness-70 shadow-[0_0_8px_rgba(0,0,0,0.2)]'>
+                <div className='h-full' onClick={handleHomeClick}><img id='logo' className='h-full w-auto' src={Logo} alt='logo' /></div>
+                <NavMenu />
+            </div>
+        </nav>
+    );
+
+    return (
+        <>
+        { isMobiile ? mobileLayout : desktopLayout }
+        </>
     )
 }
