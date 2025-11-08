6dc2c4a2c9b9a495ac4f037a4a82e57c707c0349
Aamir Husain <aamirhatim.h@gmail.com>
2025-11-07 21:03:56 -0500

    Add spacing to whole section

--- src/components/atoms/ArrowBtn.tsx
+++ src/components/atoms/ArrowBtn.tsx
@@ -7,7 +7,7 @@ export default function ArrowBtn(props:{text:string, link:string}) {
     const appContext = useAppContext()
     
     return (
-        <Link to="/projects" onClick={() => appContext.setNavSelect(props.link)} className="flex items-center gap-2 text-xl mt-5 w-max hover:gap-4 hover:ml-2 transition-[gap, ml] duration-100">
+        <Link to="/projects" onClick={() => appContext.setNavSelect(props.link)} className="flex items-center gap-2 text-xl w-max hover:gap-4 hover:ml-2 transition-[gap, ml] duration-100">
             <div>{props.text}</div>
             <FontAwesomeIcon icon={faAnglesRight} size='sm' />
         </Link>
