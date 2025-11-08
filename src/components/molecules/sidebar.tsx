998df0e2404a4789d46dd63b394800264521c696
Aamir Husain <aamirhatim.h@gmail.com>
2025-11-07 19:22:28 -0500

    add fading animations for title

--- src/components/molecules/sidebar.tsx
+++ src/components/molecules/sidebar.tsx
@@ -3,35 +3,57 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { Link } from "react-router"
 import { useAppContext } from "../../context/appContext"
 import { useEffect, useState } from "react"
+import { ANIMATION_DURATION_MS } from "../pages/AppLayout"
 
 export default function Sidebar(props: {title:string}) {
     // Get context
-    const appContext = useAppContext()
+    const { navSelect, setNavSelect } = useAppContext();
+    const { title } = props;
 
     // Init state
-    const [showSidebar, setShowSidebar] = useState<boolean>(false)
+    const [showSidebar, setShowSidebar] = useState<boolean>(false);
+    const [displayTitle, setDisplayTitle] = useState<string>(props.title);
+    const [isFading, setIsFading] = useState<boolean>(true);
 
-    const handleClose = () => {
-        appContext.setNavSelect("home")
-    }
+    // Define animation classes
+    const slideInClasses = `transform transition-all duration-500 ease-out ${showSidebar ? 'opacity-100 translate-x-0' : 'opacity-0 -translate-x-full'}`
+    const fadeClasses = `transition-opacity duration-500 ease-out ${isFading ? 'opacity-0' : 'opacity-100'}`
 
     // Hide sidebar if on home page
-    useEffect( () => {
-        if (appContext.navSelect === "home") {
+    useEffect(() => {
+        if (navSelect === "home") {
             setShowSidebar(false)
         } else {
             setShowSidebar(true)
         }
-    }, [appContext.navSelect])
+    }, [navSelect]);
+
+    // Fade animation for sidebar title
+    useEffect(() => {
+        if (displayTitle === "home") {
+            // No fade in if going from home to another page
+            setDisplayTitle(title);
+            setIsFading(false);
+        } else if (title !== displayTitle) {
+            // Fade if switching between non-home pages
+            setIsFading(true);
+            const timer = setTimeout(() => {
+                setDisplayTitle(title);
+                setIsFading(false);
+            }, ANIMATION_DURATION_MS);
+
+            return () => clearTimeout(timer);
+        } else {
+            // No animation if title is the same
+            setDisplayTitle(title);
+            setIsFading(false);
+        }
+    }, [title]);
     
     return (
-        <>
-        {showSidebar &&
-            <div id="sidebar" className="float-left box-border px-12 pt-40 pb-20 flex flex-col justify-between items-start h-full w-[300px] grow-0 shrink-0">
-                <div className="text-5xl font-bold text-violet-900">{props.title}.</div>
-                <Link to="/" onClick={handleClose} className="hover:scale-130 transition duration-100"><FontAwesomeIcon icon={faArrowAltCircleLeft} size="2xl" /></Link>
-            </div>
-        }
-        </>
+        <div id="sidebar" className={`float-left box-border px-12 pt-40 pb-20 flex flex-col justify-between items-start h-full w-[300px] grow-0 shrink-0 ${slideInClasses}`}>
+            <div className={`text-5xl font-bold text-violet-900 ${fadeClasses}`}>{displayTitle}.</div>
+            <Link to="/" onClick={() => setNavSelect("home")} className="hover:scale-130 transition duration-100"><FontAwesomeIcon icon={faArrowAltCircleLeft} size="2xl" /></Link>
+        </div>
     )
 }
