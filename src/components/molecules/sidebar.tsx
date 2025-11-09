5887abc0e94ce6fd1c271e2709a61a63f2fab450
Aamir Husain <aamirhatim.h@gmail.com>
2025-11-09 00:40:27 -0500

    add color on hover

--- src/components/molecules/sidebar.tsx
+++ src/components/molecules/sidebar.tsx
@@ -52,7 +52,7 @@ export default function Sidebar(props: {title:string}) {
     return (
         <div id="sidebar" className={`float-left box-border px-12 pt-40 pb-20 flex flex-col justify-between items-start h-full w-[300px] grow-0 shrink-0 ${slideInClasses}`}>
             <div className={`text-5xl font-bold text-violet-900 ${fadeClasses}`}>{displayTitle}.</div>
-            <div onClick={() => setNavSelect("home")} className="hover:scale-130 transition duration-100"><FontAwesomeIcon icon={faArrowAltCircleLeft} size="2xl" /></div>
+            <div onClick={() => setNavSelect("home")} className={`hover:scale-130 hover:text-[var(--txt-title-color)] transition-all duration-[${ANIMATION_DURATION_MS}ms]`}><FontAwesomeIcon icon={faArrowAltCircleLeft} size="2xl" /></div>
         </div>
     )
 }
