2a4b9bf3661ba3d305eb2dae60ee9d423493a46b
Aamir Husain <aamirhatim.h@gmail.com>
2025-11-09 00:57:13 -0500

    add cursor pointer

--- src/components/molecules/navbar.tsx
+++ src/components/molecules/navbar.tsx
@@ -26,12 +26,12 @@ export default function Navbar() {
     const desktopLayout = (
         <nav className='box-border w-dvw fixed top-0 left-0 flex justify-center p-5 z-10'>
             <div className='w-full w-max-view box-border px-10 py-3 flex items-center gap-8 rounded-full text-l backdrop-blur-md backdrop-brightness-70 shadow-[0_0_8px_rgba(0,0,0,0.2)]'>
-                <div onClick={handleHomeClick}><img id='logo' className='size-[22px]' src={Logo} alt='logo' /></div>
+                <div className='cursor-pointer' onClick={handleHomeClick}><img id='logo' className='size-[22px]' src={Logo} alt='logo' /></div>
                 
                 <div className='flex grow justify-start gap-8 h-full'>
                     {
                         navItems.map( (n, idx) => (
-                            <div key={idx} className={`content-center font-bold hover:text-[var(--txt-accent-color)] transition-colors ${appContext.navSelect === n ? 'text-[var(--txt-accent-color)]' : ''}`} onClick={() => handleNavClick(n)}>{n}</div>
+                            <div key={idx} className={`cursor-pointer content-center font-bold hover:text-[var(--txt-accent-color)] transition-colors ${appContext.navSelect === n ? 'text-[var(--txt-accent-color)]' : ''}`} onClick={() => handleNavClick(n)}>{n}</div>
                         ))
                     }
                 </div>
