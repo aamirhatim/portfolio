c3b70b4e37517897fa28dc69735b48bca30f4117
Aamir Husain <aamirhatim.h@gmail.com>
2025-11-10 15:48:35 -0500

    Pass project id to project item

--- src/components/pages/projectsPage.tsx
+++ src/components/pages/projectsPage.tsx
@@ -23,7 +23,7 @@ export default function ProjectsPage() {
     return (
         <div className={`flex flex-col gap-30`}>
             <section className="box-border flex flex-col gap-12">
-                {projectList.map( (p, idx) => <ProjectItem key={idx} project={p.data as ProjectType} /> )}
+                {projectList.map( (p, idx) => <ProjectItem key={idx} project={{...p.data, id: p.id as string} as ProjectType} /> )}
             </section>
         </div>
     )
