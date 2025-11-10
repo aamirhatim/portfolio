d1234254b55eb8a39a245a94499198f7921b26a6
Aamir Husain <aamirhatim.h@gmail.com>
2025-11-10 16:19:49 -0500

    make x padding conditional

--- src/components/molecules/sidebar.tsx
+++ src/components/molecules/sidebar.tsx
@@ -55,7 +55,7 @@ export default function Sidebar(props: {title:string}) {
     }, [title]);
     
     return (
-        <div id="sidebar" className={`float-left box-border px-12 pt-40 pb-20 flex flex-col justify-between items-start h-full grow-0 shrink-0 ${slideInClasses} ${(navToHome || navToArticle) ? "w-0" : "w-[300px]"}`}>
+        <div id="sidebar" className={`float-left box-border pt-40 pb-20 flex flex-col justify-between items-start h-full grow-0 shrink-0 ${slideInClasses} ${(navToHome || navToArticle) ? "w-0" : "px-12 w-[300px]"}`}>
             <div className={`text-5xl font-bold text-violet-900 ${fadeClasses}`}>{displayTitle}.</div>
             <div onClick={() => setNavSelect("home")} className={`hover:scale-130 hover:text-[var(--txt-title-color)] transition-all duration-[${ANIMATION_DURATION_MS}ms]`}><FontAwesomeIcon icon={faArrowAltCircleLeft} size="2xl" /></div>
         </div>
