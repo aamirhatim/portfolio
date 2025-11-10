eeb3cb2e00d649a04d815e430425ceb6ec30eeb7
Aamir Husain <aamirhatim.h@gmail.com>
2025-11-10 16:02:21 -0500

    add outlet for project viewer

--- src/components/pages/projectsPage.tsx
+++ src/components/pages/projectsPage.tsx
@@ -3,10 +3,13 @@ import { useFirebaseAppContext } from "../../context/firebaseAppContext";
 import { getDocumentsFromCollection } from "../../lib/firestoreLib"
 import ProjectItem from "../molecules/ProjectItem"
 import { FirestoreDocType, ProjectType } from "../../data/datatypes";
+import { Outlet, useLocation } from "react-router";
 
 export default function ProjectsPage() {
     // Get context
     const firebaseAppContext = useFirebaseAppContext();
+    const location = useLocation();
+    const isIndexRoute = location.pathname === '/projects' || location.pathname === '/projects/';
 
     // Init state
     const [projectList, setProjectList] = useState<FirestoreDocType[]>([]);
@@ -20,11 +23,26 @@ export default function ProjectsPage() {
         getProjects();
     }, []);
 
-    return (
+    const indexLayout = (
         <div className={`flex flex-col gap-30`}>
             <section className="box-border flex flex-col gap-12">
                 {projectList.map( (p, idx) => <ProjectItem key={idx} project={{...p.data, id: p.id as string} as ProjectType} /> )}
             </section>
         </div>
+    );
+
+    const articleLayout = (
+        <div>
+            <Outlet />
+        </div>
+    );
+
+    return (
+        <>
+        {isIndexRoute
+            ? indexLayout
+            : articleLayout
+        }
+        </>
     )
 }
