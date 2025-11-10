693d448a52e36b6022196fcc469b898b1187667a
Aamir Husain <aamirhatim.h@gmail.com>
2025-11-10 16:01:37 -0500

    add logic to hide sidebar space when viewing article

--- src/components/molecules/sidebar.tsx
+++ src/components/molecules/sidebar.tsx
@@ -13,6 +13,8 @@ export default function Sidebar(props: {title:string}) {
     const [showSidebar, setShowSidebar] = useState<boolean>(false);
     const [displayTitle, setDisplayTitle] = useState<string>(props.title);
     const [isFading, setIsFading] = useState<boolean>(true);
+    const [navToHome, setNavToHome] = useState<boolean>(navSelect === "home");
+    const [navToArticle, setNavToArticle] = useState<boolean>(navSelect !== "projects" && navSelect.startsWith("projects/"));
 
     // Define animation classes
     const slideInClasses = `transform transition-all duration-[${ANIMATION_DURATION_MS}ms] ease-in-out ${showSidebar ? 'opacity-100 translate-x-0' : 'opacity-0 -translate-x-full'}`
@@ -20,15 +22,15 @@ export default function Sidebar(props: {title:string}) {
 
     // Hide sidebar if on home page or article
     useEffect(() => {
-        const navToHome = navSelect === "home";
-        const navToArticle = navSelect !== "projects" && navSelect.startsWith("projects/");
-
         if (navToHome || navToArticle) {
             setShowSidebar(false);
         } else {
             setShowSidebar(true);
-        }
-    }, [navSelect]);
+        };
+
+        setNavToHome(navSelect === "home");
+        setNavToArticle(navSelect !== "projects" && navSelect.startsWith("projects/"));
+    }, [navSelect, navToHome, navToArticle]);
 
     // Fade animation for sidebar title
     useEffect(() => {
@@ -53,7 +55,7 @@ export default function Sidebar(props: {title:string}) {
     }, [title]);
     
     return (
-        <div id="sidebar" className={`float-left box-border px-12 pt-40 pb-20 flex flex-col justify-between items-start h-full grow-0 shrink-0 ${slideInClasses} ${navSelect === "home" ? "w-0" : "w-[300px]"}`}>
+        <div id="sidebar" className={`float-left box-border px-12 pt-40 pb-20 flex flex-col justify-between items-start h-full grow-0 shrink-0 ${slideInClasses} ${(navToHome || navToArticle) ? "w-0" : "w-[300px]"}`}>
             <div className={`text-5xl font-bold text-violet-900 ${fadeClasses}`}>{displayTitle}.</div>
             <div onClick={() => setNavSelect("home")} className={`hover:scale-130 hover:text-[var(--txt-title-color)] transition-all duration-[${ANIMATION_DURATION_MS}ms]`}><FontAwesomeIcon icon={faArrowAltCircleLeft} size="2xl" /></div>
         </div>
