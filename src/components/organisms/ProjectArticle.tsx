717aa06aa0824d248486ffbd735f485c09fba6fe
Aamir Husain <aamirhatim.h@gmail.com>
2025-11-11 22:12:01 -0500

    implement doc retrieval and add chip group

--- src/components/organisms/ProjectArticle.tsx
+++ src/components/organisms/ProjectArticle.tsx
@@ -1,7 +1,36 @@
+import { useEffect, useState } from "react"
+import { ProjectType } from "../../data/datatypes";
+import { getDocumentFromId } from "../../lib/firestoreLib";
+import { useFirebaseAppContext } from "../../context/firebaseAppContext";
+import ChipGroup from "../molecules/ChipGroup";
+
 export default function ProjectArticle(props: {projectId:string}) {
+    // Get context
+    const firebaseAppContext = useFirebaseAppContext();
+
+    // Init state
+    const [project, setProject] = useState<ProjectType>();
+
+    // Get project info
+    useEffect(() => {
+        const getProject = async () => {
+            const projectDoc = await getDocumentFromId(firebaseAppContext, "projects", props.projectId);
+            if (!projectDoc) return;
+
+            const project = {
+                id: projectDoc.id,
+                ...projectDoc.data,
+            } as ProjectType;
+            setProject(project);
+        };
+
+        getProject();
+    }, [props.projectId]);
+
     return (
-        <div className="box-border flex flex-col gap-12 bg-red-400">
-            {props.projectId}
+        <div className="box-border flex flex-col gap-12">
+            <div className="font-bold text-6xl text-(--txt-feature-color)">{project?.title}</div>
+            {project?.skills && <ChipGroup list={project.skills} />}
         </div>
     )
 }
