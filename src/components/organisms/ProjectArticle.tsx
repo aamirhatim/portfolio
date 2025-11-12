b2606b933b7b8f32e9ed65f80ab67fdcf8d68cb4
Aamir Husain <aamirhatim.h@gmail.com>
2025-11-11 22:31:40 -0500

    get article blocks

--- src/components/organisms/ProjectArticle.tsx
+++ src/components/organisms/ProjectArticle.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react"
-import { ProjectType } from "../../data/datatypes";
+import { ProjectArticleType, ProjectType } from "../../data/datatypes";
 import { getDocumentFromId } from "../../lib/firestoreLib";
 import { useFirebaseAppContext } from "../../context/firebaseAppContext";
 import ChipGroup from "../molecules/ChipGroup";
@@ -10,6 +10,7 @@ export default function ProjectArticle(props: {projectId:string}) {
 
     // Init state
     const [project, setProject] = useState<ProjectType>();
+    const [article, setArticle] = useState<ProjectArticleType>();
 
     // Get project info
     useEffect(() => {
@@ -17,20 +18,47 @@ export default function ProjectArticle(props: {projectId:string}) {
             const projectDoc = await getDocumentFromId(firebaseAppContext, "projects", props.projectId);
             if (!projectDoc) return;
 
-            const project = {
+            const projectData = {
                 id: projectDoc.id,
                 ...projectDoc.data,
             } as ProjectType;
-            setProject(project);
+            setProject(projectData);
         };
 
         getProject();
     }, [props.projectId]);
 
+    // Get project article
+    useEffect(() => {
+        const getProjectArticle = async () => {
+            const doc = await getDocumentFromId(firebaseAppContext, "project_articles", props.projectId);
+            if (!doc) return;
+
+            const articleData = {
+                ...doc.data
+            } as ProjectArticleType;
+            setArticle(articleData);
+        };
+
+        getProjectArticle();
+    }, [props.projectId]);
+
     return (
         <div className="box-border flex flex-col gap-12">
             <div className="font-bold text-6xl text-(--txt-feature-color)">{project?.title}</div>
-            {project?.skills && <ChipGroup list={project.skills} />}
+
+            {article &&
+                <>
+                <div className="text-md text-(--txt-feature-color)">{article.publishDate}</div>
+                </>
+            }
+
+            {project?.skills &&
+                <div>
+                    <div className="font-bold text-lg text-(--txt-feature-color) mb-3">Keywords</div>
+                    <ChipGroup list={project.skills} />
+                </div>
+            }
         </div>
     )
 }
