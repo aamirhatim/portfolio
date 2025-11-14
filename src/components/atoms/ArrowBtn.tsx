2935bcd8c56f0295670eb4888dd62c4d6e298c0b
Aamir Husain <aamirhatim.h@gmail.com>
2025-11-14 16:06:34 -0500

    change color of arrow button

--- src/components/atoms/ArrowBtn.tsx
+++ src/components/atoms/ArrowBtn.tsx
@@ -7,7 +7,7 @@ export default function ArrowBtn(props:{text:string, link:string}) {
     const appContext = useAppContext()
     
     return (
-        <Link to="/projects" onClick={() => appContext.setNavSelect(props.link)} className="flex items-center gap-2 text-xl w-max hover:gap-4 hover:ml-2 transition-[gap, ml] duration-100">
+        <Link to="/projects" onClick={() => appContext.setNavSelect(props.link)} className="!text-(--txt-feature-color) !no-underline flex items-center gap-2 text-xl w-max hover:gap-4 hover:ml-2 transition-[gap, ml] duration-100">
             <div>{props.text}</div>
             <FontAwesomeIcon icon={faAnglesRight} size='sm' />
         </Link>
