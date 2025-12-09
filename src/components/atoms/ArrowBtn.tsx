2825f89a08a2a06ee0abca11fefae26304458931
Aamir Husain <aamirhatim.h@gmail.com>
2025-12-09 17:22:17 -0500

    make arrow btn more flexible and accept links that can be opened in a new tab

--- src/components/atoms/ArrowBtn.tsx
+++ src/components/atoms/ArrowBtn.tsx
@@ -1,18 +1,29 @@
-import { Link } from "react-router";
+import { useNavigate } from "react-router";
 import { useAppContext } from "../../context/appContext";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAnglesRight } from "@fortawesome/free-solid-svg-icons";
 import { ANIMATION_DURATION_MS } from "../../data/constants";
 
-export default function ArrowBtn(props:{text:string, link:string, className?:string}) {
-    const appContext = useAppContext();
+export default function ArrowBtn(props:{text:string, link:string, className?:string, newTab?:boolean}) {
+    const navigate = useNavigate();
+    const { setNavSelect } = useAppContext();
+
+    // Nav handler
+    const handleNav = () => {
+        if (props.newTab) {
+            window.open(props.link, '_blank');
+        } else {
+            setNavSelect(props.link);
+            navigate(props.link);
+        }
+    };
 
     const hoverClasses = `hover:gap-4 hover:ml-2 transition-all duration-[${ANIMATION_DURATION_MS}ms] ease-in-out`;
     
     return (
-        <Link to="/projects" onClick={() => appContext.setNavSelect(props.link)} className={`${props.className} w-fit !no-underline flex items-center gap-2 ${hoverClasses}`}>
+        <div onClick={handleNav} className={`${props.className} cursor-pointer w-fit !no-underline flex items-center gap-2 ${hoverClasses}`}>
             <div>{props.text}</div>
             <FontAwesomeIcon icon={faAnglesRight} size='sm' />
-        </Link>
+        </div>
     )
 }
