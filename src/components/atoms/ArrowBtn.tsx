31abab7d20833ba384014e592437793087079f20
Aamir Husain <aamirhatim.h@gmail.com>
2025-12-09 17:10:02 -0500

    add option to provide tailwind styling

--- src/components/atoms/ArrowBtn.tsx
+++ src/components/atoms/ArrowBtn.tsx
@@ -1,13 +1,16 @@
-import { Link } from "react-router"
-import { useAppContext } from "../../context/appContext"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faAnglesRight } from "@fortawesome/free-solid-svg-icons"
+import { Link } from "react-router";
+import { useAppContext } from "../../context/appContext";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAnglesRight } from "@fortawesome/free-solid-svg-icons";
+import { ANIMATION_DURATION_MS } from "../../data/constants";
 
-export default function ArrowBtn(props:{text:string, link:string}) {
-    const appContext = useAppContext()
+export default function ArrowBtn(props:{text:string, link:string, className?:string}) {
+    const appContext = useAppContext();
+
+    const hoverClasses = `hover:gap-4 hover:ml-2 transition-all duration-[${ANIMATION_DURATION_MS}ms] ease-in-out`;
     
     return (
-        <Link to="/projects" onClick={() => appContext.setNavSelect(props.link)} className="!text-(--txt-feature-color) !no-underline flex items-center gap-2 text-xl w-max hover:gap-4 hover:ml-2 transition-[gap, ml] duration-100">
+        <Link to="/projects" onClick={() => appContext.setNavSelect(props.link)} className={`${props.className} w-fit !no-underline flex items-center gap-2 ${hoverClasses}`}>
             <div>{props.text}</div>
             <FontAwesomeIcon icon={faAnglesRight} size='sm' />
         </Link>
