8da91b4cb302afa143d8a3164d963a2fe570c05e
Aamir Husain <aamirhatim.h@gmail.com>
2025-12-09 13:41:14 -0500

    add mobile styling

--- src/components/pages/projectsPage.tsx
+++ src/components/pages/projectsPage.tsx
@@ -1,15 +1,20 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useFirebaseAppContext } from "../../context/firebaseAppContext";
 import { getDocumentsFromCollection } from "../../lib/firestoreLib"
 import ProjectItem from "../molecules/ProjectItem"
 import { FirestoreDocType, ProjectType } from "../../data/datatypes";
+import useIsMobile from "../hooks";
 
 export default function ProjectsPage() {
     // Get context
     const firebaseAppContext = useFirebaseAppContext();
+    const isMobile = useIsMobile();
 
     // Init state
     const [projectList, setProjectList] = useState<FirestoreDocType[]>([]);
+
+    // Create refs
+    const sectionRef = useRef<HTMLDivElement>(null);
 
     // Get list of projects
     useEffect( () => {
@@ -25,10 +30,10 @@ export default function ProjectsPage() {
     }, []);
 
     return (
-        <div className={`flex flex-col gap-30`}>
-            <section className="box-border flex flex-col gap-12">
-                {projectList.map( (p, idx) => <ProjectItem key={idx} project={{...p.data, id: p.id as string} as ProjectType} /> )}
-            </section>
-        </div>
+        <section ref={sectionRef} className={`box-border pt-40 flex flex-col mx-auto ${isMobile ? 'px-4 gap-6' : 'gap-18 max-w-[1000px]'}`}>
+            {projectList.map((p, idx) => {
+                return <ProjectItem key={idx} project={{...p.data, id: p.id as string} as ProjectType} idx={idx} />
+            })}
+        </section>
     )
 }
