5b0e14137594ba1d099cfffd00a4004e16446170
Aamir Husain <aamirhatim.h@gmail.com>
2025-12-09 10:37:13 -0500

    Update button style to use icons. Add blog button

--- src/components/atoms/ProjectLink.tsx
+++ src/components/atoms/ProjectLink.tsx
@@ -1,12 +1,31 @@
+import { faVideo, faNewspaper, faLink, faCodeBranch } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 export default function ProjectLink(props: {value:string, url:string}) {
     const handleClick = (e:React.MouseEvent) => {
         e.stopPropagation();
         window.open(props.url, '_blank');
     };
 
+    const getIcon = () => {
+        switch (props.value) {
+            case "code":
+                return faCodeBranch;
+
+            case "video":
+                return faVideo;
+
+            case "blog":
+                return faNewspaper;
+        
+            default:
+                return faLink;
+        }
+    };
+
     return (
-        <div onClick={handleClick} className='text-sm font-bold text-(--txt-link-color) cursor-pointer'>
-            <a className="!no-underline">{`{ ${props.value} }`}</a>
+        <div onClick={handleClick} className='p-1 cursor-pointer'>
+            <a><FontAwesomeIcon icon={getIcon()} size='sm' className='text-(--txt-subtitle-color)' /></a>
         </div>
     )
 }
