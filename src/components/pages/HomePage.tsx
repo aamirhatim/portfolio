3f995396cd0bd58f9728aad8a5bafd52b2a931c5
Aamir Husain <aamirhatim.h@gmail.com>
2025-12-10 18:52:13 -0500

    update spacing

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -47,15 +47,15 @@ export default function HomePage() {
 
     return (
         <div className="box-border flex flex-col w-full gap-40">
-            <div className={`feature px-6 flex text-[var(--txt-feature-color)] ${isMobile ? 'text-5xl' : 'text-6xl w-[65%]'}`}>{introTxt}</div>
+            <div className={`feature px-4 flex text-(--txt-feature-color) ${isMobile ? 'text-5xl' : 'text-6xl w-[65%]'}`}>{introTxt}</div>
 
             <section className="flex flex-col gap-8">
-                <div className="title text-3xl px-6">Featured work</div>
+                <div className="title text-3xl px-4">Featured work</div>
                 <div className={`flex flex-col gap-4`}>
                     {projSpotlightList.map((p, idx) => <ProjectHighlight key={idx} project={{id: p.id, ...p.data} as ProjectType} idx={idx} />)}
                 </div>
                 
-                <div className={`w-full flex pl-6 text-lg`}>
+                <div className={`w-full flex text-lg px-4`}>
                     <ArrowBtn text="See more" link="/projects" className="!text-(--txt-subtitle-color)"/>
                 </div>
             </section>
