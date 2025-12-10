af6216179ed982133935b3e0a34d414545696622
Aamir Husain <aamirhatim.h@gmail.com>
2025-12-10 11:45:01 -0500

    import logo using svgr method

--- src/components/molecules/navbar.tsx
+++ src/components/molecules/navbar.tsx
@@ -1,5 +1,5 @@
 import { useNavigate } from 'react-router'
-import Logo from '../../assets/aamirhatim_logo.svg'
+import Logo from '../../assets/logo.svg?react'
 import { useAppContext } from '../../context/appContext'
 import useIsMobile from '../hooks'
 import NavMenu from './NavMenu'
@@ -31,7 +31,7 @@ export default function Navbar() {
     const desktopLayout = (
         <nav className='box-border w-dvw fixed top-0 left-0 px-10 flex justify-center z-10 backdrop-blur-md bg-(--bg-color)/60'>
             <div className='w-full w-max-view box-border py-6 flex items-center gap-8'>
-                <div className='cursor-pointer' onClick={() => handleNavClick("home")}><img id='logo' className='size-[22px]' src={Logo} alt='logo' /></div>
+                <Logo className='cursor-pointer h-5 w-auto fill-(--txt-body-color)' onClick={() => handleNavClick("home")} />
                 
                 <div className='flex grow justify-start gap-8 h-full'>
                     {
@@ -49,7 +49,7 @@ export default function Navbar() {
     const mobileLayout = (
         <nav className='box-border w-dvw fixed top-0 left-0 flex justify-center z-10 backdrop-blur-md bg-(--bg-color)/60'>
             <div className='w-full w-max-view box-border px-6 py-3 flex items-center justify-between gap-8'>
-                <div className='h-full cursor-pointer' onClick={() => handleNavClick("home")}><img id='logo' className='box-border p-1 h-full w-auto' src={Logo} alt='logo' /></div>
+                <Logo className='cursor-pointer h-5 w-auto fill-(--txt-body-color)' onClick={() => handleNavClick("home")} />
                 <NavMenu />
             </div>
         </nav>
