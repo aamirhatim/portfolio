e38521f8beadcbac18d0da0100a4a6a1b42d005e
Aamir Husain <aamirhatim.h@gmail.com>
2025-12-11 12:58:25 -0500

    parse colors for animation compatibility

--- src/components/atoms/ArrowBtn.tsx
+++ src/components/atoms/ArrowBtn.tsx
@@ -3,6 +3,7 @@ import { useAppContext } from "../../context/appContext";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAnglesRight } from "@fortawesome/free-solid-svg-icons";
 import { motion } from "motion/react";
+import Color from "colorjs.io";
 
 export default function ArrowBtn(props:{text:string, link:string, className?:string, newTab?:boolean}) {
     const navigate = useNavigate();
@@ -19,10 +20,22 @@ export default function ArrowBtn(props:{text:string, link:string, className?:str
     };
 
     // Define animation config
+    const styles = window.getComputedStyle(document.body);
+    const initColorVar = styles.getPropertyValue('--txt-subtitle-color');
+    const initColor = new Color(initColorVar);
+    const initColorHex = initColor.to('srgb').toString({format: 'hex'});
+
+    const highlightVar = styles.getPropertyValue('--txt-highlight-color');
+    const highlightColor = new Color(highlightVar);
+    const highlightHex = highlightColor.to('srgb').toString({format: 'hex'});
+    
+    const initial = {
+        color: initColorHex
+    }
     const hover = {
         gap: '16px',
         marginLeft: '16px',
-        color: 'var(--txt-highlight-color) !important',
+        color: highlightHex,
         transition: { duration: .15 }
     }
     const tap = {
@@ -34,6 +47,7 @@ export default function ArrowBtn(props:{text:string, link:string, className?:str
         <motion.div
             onClick={handleNav}
             className={`${props.className} cursor-pointer w-fit !no-underline flex items-center gap-2`}
+            initial={initial}
             whileHover={hover}
             whileTap={tap}
         >
