b7ff04e9d62c47454c395a0fc53e58054b58eb8d
Aamir Husain <aamirhatim.h@gmail.com>
2025-12-10 19:00:04 -0500

    update classes

--- src/components/pages/projectsPage.tsx
+++ src/components/pages/projectsPage.tsx
@@ -27,7 +27,7 @@ export default function ProjectsPage() {
     }, []);
 
     return (
-        <section className={`box-border flex flex-col mx-auto px-4 ${isMobile ? 'gap-6' : 'gap-18 max-w-[800px]'}`}>
+        <section className={`box-border flex flex-col px-4 ${isMobile ? 'gap-6 w-full' : 'gap-18 max-w-[800px] mx-auto'}`}>
             {projectList.map((p, idx) => <ProjectItem key={idx} project={{...p.data, id: p.id as string} as ProjectType} />)}
         </section>
     )
