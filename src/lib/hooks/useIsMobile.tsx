39557864e0888d4312383682791bc8a14cd9b8ae
Aamir Husain <aamirhatim.h@gmail.com>
2025-12-13 01:31:34 -0500

    reduce frequency of screen size checks

--- src/lib/hooks/useIsMobile.tsx
+++ src/lib/hooks/useIsMobile.tsx
@@ -1,21 +1,32 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 const MOBILE_BREAKPOINT_PX = 768;
+const DEBOUNCE_DELAY_MS = 200;
+
+// Define debounce function
+const debounce = (f: Function, delay: number) => {
+    let timeout: NodeJS.Timeout;
+    return (...args: any) => {
+        clearTimeout(timeout);
+        timeout = setTimeout(() => f(...args), delay);
+    };
+};
 
 const useIsMobile = () => {
-    const [isMobile, setIsMobile] = useState<boolean>(
-        typeof window !== 'undefined' ? window.innerWidth < MOBILE_BREAKPOINT_PX : false
-    );
+    const [isMobile, setIsMobile] = useState<boolean>(false);   // Assume default view is desktop
+
+    // Define helper to check screen size
+    const checkScreenSize = useCallback(() => {
+        setIsMobile(window.innerWidth < MOBILE_BREAKPOINT_PX);
+    }, []);
 
     useEffect(() => {
-        const checkScreenSize = () => {
-            setIsMobile(window.innerWidth < MOBILE_BREAKPOINT_PX);
-        };
-
-        window.addEventListener('resize', checkScreenSize);
-        checkScreenSize();
-        return () => window.removeEventListener('resize', checkScreenSize);
-    }, []);
+        if (typeof window === 'undefined') return;                              // Check if window exists
+        const debouncedCheck = debounce(checkScreenSize, DEBOUNCE_DELAY_MS);    // Apply debounce
+        window.addEventListener('resize', debouncedCheck);                      // Add listener
+        checkScreenSize();                                                      // Check screen size
+        return () => window.removeEventListener('resize', debouncedCheck);      // Cleanup
+    }, [checkScreenSize]);
 
     return isMobile;
 };
