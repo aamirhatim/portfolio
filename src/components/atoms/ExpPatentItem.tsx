7f09a24134dae1d64527cace7170d69c4f8e81a2
Aamir Husain <aamirhatim.h@gmail.com>
2026-01-09 09:08:45 -0500

    remove animations

--- src/components/atoms/ExpPatentItem.tsx
+++ src/components/atoms/ExpPatentItem.tsx
@@ -1,39 +1,18 @@
-import { motion } from 'motion/react'
 import { PatentType } from '../../data/datatypes'
 
-export default function ExpPatentItem(props: {item: PatentType}) {
-    // Animation config
-    const initial = {
-        opacity: 0,
-        y: 50
-    }
-    const whileInView = {
-        opacity: 1,
-        y: 0,
-        transition: { duration: .2 }
-    }
-    const viewport = {
-        once: true,
-        amount: .5
-    }
-
+export default function ExpPatentItem(props: { item: PatentType }) {
     return (
-        <motion.div
-            className='box-border flex flex-col justify-start gap-2'
-            initial={initial}
-            whileInView={whileInView}
-            viewport={viewport}
-        >
+        <div className='box-border flex flex-col justify-start gap-2'>
             <div className='flex gap-3 items-center'>
                 <div className='box-border border rounded-sm title text-sm text-(--txt-subtitle-color) px-2 w-min'>{props.item.status.toUpperCase()}</div>
-                {props.item.status === "granted" && 
-                <a href={props.item.url} target='_blank'>
-                    <div className='text-sm underline'>US Patent No. {props.item.number}</div>
-                </a>
+                {props.item.status === "granted" &&
+                    <a href={props.item.url} target='_blank'>
+                        <div className='text-sm underline'>US Patent No. {props.item.number}</div>
+                    </a>
                 }
             </div>
             <div className='text-2xl title'>{props.item.title}</div>
             <div className='text-lg'>{props.item.description}</div>
-        </motion.div> 
+        </div>
     )
 }
