d70ce37da103034b42955a3cc6e32a9613774a0f
Aamir Husain <aamirhatim.h@gmail.com>
2026-01-09 08:47:52 -0500

    remove animation config. This will now be handled by a wrapper component

--- src/components/atoms/ExpJobItem.tsx
+++ src/components/atoms/ExpJobItem.tsx
@@ -1,30 +1,9 @@
-import { motion } from 'motion/react'
 import { JobType } from '../../data/datatypes'
 import ChipGroup from '../molecules/ChipGroup'
 
-export default function ExpJobItem(props: {job: JobType}) {
-    // Animation config
-    const initial = {
-        opacity: 0,
-        y: 50
-    }
-    const whileInView = {
-        opacity: 1,
-        y: 0,
-        transition: { duration: .2 }
-    }
-    const viewport = {
-        once: true,
-        amount: .5
-    }
-    
+export default function ExpJobItem(props: { job: JobType }) {
     return (
-        <motion.div
-            className='flex flex-col gap-2'
-            initial={initial}
-            whileInView={whileInView}
-            viewport={viewport}
-        >
+        <div className='flex flex-col gap-2'>
             <div className='text-sm text-(--txt-subtitle-color)'>{props.job.start} - {props.job.end}</div>
             <div className='text-2xl title'>{props.job.title} <span className='text-(--txt-subtitle-color)'>@{props.job.company}</span></div>
             <div className='text-lg'>{props.job.description}</div>
@@ -33,6 +12,6 @@ export default function ExpJobItem(props: {job: JobType}) {
                     <ChipGroup list={props.job.skills} />
                 </div>
             }
-        </motion.div> 
+        </div>
     )
 }
