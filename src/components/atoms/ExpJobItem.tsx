8e2df73be43830c370d0ba8fff4098fb13c440fd
Aamir Husain <aamirhatim.h@gmail.com>
2026-01-09 08:58:40 -0500

    add flag to either show detail or description

--- src/components/atoms/ExpJobItem.tsx
+++ src/components/atoms/ExpJobItem.tsx
@@ -1,12 +1,12 @@
 import { JobType } from '../../data/datatypes'
 import ChipGroup from '../molecules/ChipGroup'
 
-export default function ExpJobItem(props: { job: JobType }) {
+export default function ExpJobItem(props: { job: JobType, showDetail?: boolean }) {
     return (
         <div className='flex flex-col gap-2'>
             <div className='text-sm text-(--txt-subtitle-color)'>{props.job.start} - {props.job.end}</div>
             <div className='text-2xl title'>{props.job.title} <span className='text-(--txt-subtitle-color)'>@{props.job.company}</span></div>
-            <div className='text-lg'>{props.job.description}</div>
+            <div className='text-lg'>{props.showDetail ? props.job.detail : props.job.description}</div>
             {props.job.skills &&
                 <div className='mt-3'>
                     <ChipGroup list={props.job.skills} />
