3f9036f488341bec5413c7ea8949ce4f32640fbd
Aamir Husain <aamirhatim.h@gmail.com>
2026-01-09 09:14:08 -0500

    simplify components by using animation wrapper

--- src/components/atoms/ExpEduItem.tsx
+++ src/components/atoms/ExpEduItem.tsx
@@ -1,31 +1,12 @@
-import { motion } from 'motion/react'
 import { EducationType } from '../../data/datatypes'
 
-export default function ExpEduItem(props: {item:EducationType}){
-    // Animation config
-    const initial = {
-        opacity: 0,
-        y: 50
-    }
-    const whileInView = {
-        opacity: 1,
-        y: 0,
-        transition: { duration: .2 }
-    }
-    const viewport = {
-        once: true,
-        amount: .5
-    }
-    
+export default function ExpEduItem(props: { item: EducationType }) {
     return (
-        <motion.div
+        <div
             className='flex flex-col gap-2'
-            initial={initial}
-            whileInView={whileInView}
-            viewport={viewport}
         >
             <div className='text-lg text-(--txt-subtitle-color)'>{props.item.school} - {props.item.end}</div>
             <div className='text-2xl title'>{props.item.degree.long}: <span className='text-(--txt-subtitle-color)'>{props.item.field}</span></div>
-        </motion.div>
+        </div>
     )
 }
--- src/components/molecules/schooling.tsx
+++ src/components/molecules/schooling.tsx
@@ -4,7 +4,7 @@ import ExpEduItem from '../atoms/ExpEduItem'
 import { EducationType, FirestoreDocType } from '../../data/datatypes';
 import { getDocumentsFromCollection } from '../../lib/firestoreLib';
 import { orderBy } from 'firebase/firestore';
-import { motion } from 'motion/react';
+import AnimateInView from '../atoms/AnimateInView';
 
 export default function Schooling() {
     // Get context
@@ -12,21 +12,6 @@ export default function Schooling() {
 
     // Init state
     const [eduList, setEduList] = useState<FirestoreDocType[]>([]);
-
-    // Animation config
-    const initial = {
-        opacity: 0,
-        y: 50
-    }
-    const whileInView = {
-        opacity: 1,
-        y: 0,
-        transition: { duration: .2 }
-    }
-    const viewport = {
-        once: true,
-        amount: .1
-    }
 
     // Helper to fetch school items
     const getEdu = useCallback(async () => {
@@ -39,25 +24,20 @@ export default function Schooling() {
     }, [firebaseAppContext, setEduList]);
 
     // Get list of education
-    useEffect( () => {
+    useEffect(() => {
         getEdu();
     }, [getEdu]);
 
     return (
         <>
-        {eduList.length > 0 &&
-            <motion.section
-                className='flex flex-col gap-15'
-                initial={initial}
-                whileInView={whileInView}
-                viewport={viewport}
-            >
-                <div className='title text-4xl text-(--txt-title-color)'>education.</div>
-                <div className='flex flex-col gap-10'>
-                    {eduList.map( (edu, idx) => <ExpEduItem key={idx} item={edu.data as EducationType} /> )}
-                </div>
-            </motion.section>
-        }
+            {eduList.length > 0 &&
+                <section className='flex flex-col gap-15'>
+                    <AnimateInView><div className='title text-4xl text-(--txt-title-color)'>education.</div></AnimateInView>
+                    <div className='flex flex-col gap-10'>
+                        {eduList.map((edu, idx) => <AnimateInView key={idx}><ExpEduItem item={edu.data as EducationType} /></AnimateInView>)}
+                    </div>
+                </section>
+            }
         </>
     )
 }
